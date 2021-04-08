b213fe42b8a998dc281ee97ab35a4f93af882de0
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-04-08 16:28:50 +1000

    Add linters configs

---
+++ scripts/joint_calling.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""
+Drive the joint calling workflow
+"""
+
+import subprocess
+
+subprocess.run(
+    'batch_workflow.py '
+    '--callset tog-wgs '
+    '--version v0 '
+    '--output_bucket gs://cpg-tob-wgs-temporary/v0/ '
+    '--keep_scratch" ',
+    shell=True,
+    check=False,
+)
