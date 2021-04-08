8ea13c8059b71fdd0302d33b90e6212c1b3885ec
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-04-08 11:19:20 +1000

    Copy QC metadata to test bucket (#3)

--- scripts/copy_test_gvcfs.py
+++ scripts/copy_test_gvcfs.py
@@ -6,6 +6,17 @@ import subprocess
 output = os.getenv('OUTPUT')
 assert output and output.startswith('gs://cpg-tob-wgs-test/')
 
+SUBSET_RE = 'TOB15[2-3]'
+QC_METADATA_FILE_NAME = 'R_210315_BINKAN1_1K1KDNA_M001.csv'
+
 subprocess.run(
-    ['gsutil', 'cp', 'gs://cpg-tob-wgs-main/v0/TOB15[2-3]?.g.vcf.gz*', output]
+    ['gsutil', 'cp', f'gs://cpg-tob-wgs-main/v0/{SUBSET_RE}?.g.vcf.gz*', output]
+)
+
+# Copy metadata subset.
+subprocess.run(
+    f'gsutil cat gs://cpg-tob-wgs-main/v0/{QC_METADATA_FILE_NAME} | '
+    f'grep -e "sample\\|{SUBSET_RE}" | '  # Also copy header.
+    f'gsutil cp - {output}/{QC_METADATA_FILE_NAME}',
+    shell=True,
 )
