1050e7d483d89d599b4fa43a4e2e15b891fbfd9e
pdiakumis <peterdiakumis@gmail.com>
2021-04-10 16:05:14 +1000

    copy scRNA tarball to temp bucket

--- scripts/copy_snpchip_vcf.py
+++ scripts/copy_snpchip_vcf.py
@@ -1,6 +1,6 @@
 """
-Copies SNPchip VCF (transferred from GWCCG in 2021-Mar-17) to the tob-wgs
-temporary bucket.
+Copies SNPchip VCF and scRNA tarball (transferred from GWCCG in 2021-Mar-17)
+to the tob-wgs temporary bucket.
 """
 
 import os
@@ -9,6 +9,7 @@ import subprocess
 output = os.getenv('OUTPUT')
 assert output and output.startswith('gs://cpg-tob-wgs-temporary/')
 
-INPUT_VCF = 'gs://cpg-tob-wgs-snpchipdata/data/onek1k_pre_imputation_genotypes.vcf.gz'
+# INPUT_VCF = 'gs://cpg-tob-wgs-snpchipdata/data/onek1k_pre_imputation_genotypes.vcf.gz'
+INPUT_TARBALL = 'gs://cpg-tob-wgs-snpchipdata/data/OneK1K_scRNA_GenotypingData.tar.gz'
 
-subprocess.run(['gsutil', 'cp', INPUT_VCF, output], check=False)
+subprocess.run(['gsutil', 'cp', INPUT_TARBALL, output], check=False)
