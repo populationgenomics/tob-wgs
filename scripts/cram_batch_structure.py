2ca9f3aeeae91b47f81802301d89016208dffc93
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-04-13 20:22:35 +1000

    Fix typo

--- scripts/cram_batch_structure.py
+++ scripts/cram_batch_structure.py
@@ -8,7 +8,7 @@ assert output and output == 'gs://cpg-tob-wgs-archive/cram/batch0/'
 
 subprocess.run(['gsutil', 'mv', 'gs://cpg-tob-wgs-archive/v0/*', output], check=True)
 
-# Copy missing sample TOB521.
+# Copy missing sample TOB1521.
 subprocess.run(
     ['gsutil', 'mv', 'gs://cpg-tob-wgs-upload/TOB1521.cram*', output], check=True
 )
