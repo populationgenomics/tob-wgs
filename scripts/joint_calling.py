13bd808147bb992452dad95e79a959dd738bf99b
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-04-15 13:31:49 +1000

    Add test argument

--- scripts/joint_calling.py
+++ scripts/joint_calling.py
@@ -5,27 +5,31 @@ Drive the joint calling workflow
 """
 
 import subprocess
+import sys
 
 subprocess.run(
-    'wget https://raw.githubusercontent.com/populationgenomics/joint-calling/develop/workflows/batch_workflow.py',
+    'wget https://raw.githubusercontent.com/populationgenomics/joint-calling/develop/workflows/batch_workflow.py -O batch_workflow.py',
     shell=True,
     check=False,
 )
 subprocess.run('mkdir joint_calling', shell=True, check=False)
 subprocess.run(
-    'wget https://raw.githubusercontent.com/populationgenomics/joint-calling/develop/joint_calling/utils.py joint_calling',
+    'wget https://raw.githubusercontent.com/populationgenomics/joint-calling/develop/joint_calling/utils.py -O joint_calling/utils.py',
     shell=True,
     check=False,
 )
+is_test = len(sys.argv) > 1 and sys.argv[1] == 'test'
 subprocess.run(
     'python batch_workflow.py '
-    '--callset tog-wgs '
-    '--version v1 '
-    '--batch 0 --batch 1 '
-    '--from main '
-    '--to temporary '
-    '--keep_scratch '
-    '--billing-project tob-wgs-standard ',
+    + '--callset tob-wgs '
+    + '--version v0 '
+    + ('--batch 0 ' if is_test else '--batch 0 --batch 1 ')
+    + '--from '
+    + ('test ' if is_test else ' main')
+    + '--to '
+    + ('temporary ' if is_test else ' analysis')
+    + '--keep-scratch '
+    + '--billing-project tob-wgs ',
     shell=True,
     check=False,
 )
