37960b9e268af153926942cdbf3d0f133167160d
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-04-15 14:03:03 +1000

    Use run_cmd

--- scripts/joint_calling.py
+++ scripts/joint_calling.py
@@ -7,20 +7,27 @@ Drive the joint calling workflow
 import subprocess
 import sys
 
-subprocess.run(
-    'wget https://raw.githubusercontent.com/populationgenomics/joint-calling/develop/workflows/batch_workflow.py -O batch_workflow.py',
-    shell=True,
-    check=False,
+
+def run_cmd(cmd):
+    """Print the command and run"""
+    print(cmd)
+    subprocess.run(
+        cmd,
+        shell=True,
+        check=False,
+    )
+
+
+run_cmd(
+    'wget https://raw.githubusercontent.com/populationgenomics/joint-calling/develop/workflows/batch_workflow.py -O batch_workflow.py'
 )
-subprocess.run('mkdir joint_calling', shell=True, check=False)
-subprocess.run(
-    'wget https://raw.githubusercontent.com/populationgenomics/joint-calling/develop/joint_calling/utils.py -O joint_calling/utils.py',
-    shell=True,
-    check=False,
+run_cmd('mkdir joint_calling')
+run_cmd(
+    'wget https://raw.githubusercontent.com/populationgenomics/joint-calling/develop/joint_calling/utils.py -O joint_calling/utils.py'
 )
 is_test = len(sys.argv) > 1 and sys.argv[1] == 'test'
-subprocess.run(
-    'python batch_workflow.py '
+run_cmd(
+    'PYTHONPATH=$PWD python batch_workflow.py '
     + '--callset tob-wgs '
     + '--version v0 '
     + ('--batch 0 ' if is_test else '--batch 0 --batch 1 ')
@@ -29,7 +36,5 @@ subprocess.run(
     + '--to '
     + ('temporary ' if is_test else ' analysis')
     + '--keep-scratch '
-    + '--billing-project tob-wgs ',
-    shell=True,
-    check=False,
+    + '--billing-project tob-wgs '
 )
