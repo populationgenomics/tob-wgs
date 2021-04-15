7f4a1d2647a9f566502f84f97355b17126539c3f
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-04-15 13:05:06 +1000

    Wget batcH_workflow from github

--- scripts/joint_calling.py
+++ scripts/joint_calling.py
@@ -7,11 +7,25 @@ Drive the joint calling workflow
 import subprocess
 
 subprocess.run(
-    'batch_workflow.py '
+    'wget https://raw.githubusercontent.com/populationgenomics/joint-calling/develop/workflows/batch_workflow.py',
+    shell=True,
+    check=False,
+)
+subprocess.run('mkdir joint_calling', shell=True, check=False)
+subprocess.run(
+    'wget https://raw.githubusercontent.com/populationgenomics/joint-calling/develop/joint_calling/utils.py joint_calling',
+    shell=True,
+    check=False,
+)
+subprocess.run(
+    'python batch_workflow.py '
     '--callset tog-wgs '
-    '--version v0 '
-    '--output_bucket gs://cpg-tob-wgs-temporary/v0/ '
-    '--keep_scratch',
+    '--version v1 '
+    '--batch 0 --batch 1 '
+    '--from main '
+    '--to temporary '
+    '--keep_scratch '
+    '--billing-project tob-wgs-standard ',
     shell=True,
     check=False,
 )
