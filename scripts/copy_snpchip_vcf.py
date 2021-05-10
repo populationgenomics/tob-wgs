035677b91e5ba9758988913c7474f6d2218a5e4c
Peter Diakumis <peterdiakumis@gmail.com>
2021-05-10 11:51:35 +1000

    Copy SNPchip data to main bucket (#10)
    * copy snpchip vcf to temp bucket
    
    * copy scRNA tarball to temp bucket
    
    * copy snpchip data to main bucket

---
+++ scripts/copy_snpchip_vcf.py
@@ -0,0 +1,16 @@
+"""
+Copies raw SNPchip genotype data tarball and Plink-generated VCF
+(transferred from GWCCG in 2021-Mar-17) to the tob-wgs main bucket.
+"""
+
+import os
+import subprocess
+
+output = os.getenv('OUTPUT')
+assert output and output.startswith('gs://cpg-tob-wgs-main/snpchip/v1')
+
+INPUT_BUCKET = 'gs://cpg-tob-wgs-snpchipdata'
+INPUT_VCF = f'{INPUT_BUCKET}/data/onek1k_pre_imputation_genotypes.vcf.gz'
+INPUT_TARBALL = f'{INPUT_BUCKET}/data/OneK1K_scRNA_GenotypingData.tar.gz'
+
+subprocess.run(['gsutil', 'cp', INPUT_VCF, INPUT_TARBALL, output], check=False)
