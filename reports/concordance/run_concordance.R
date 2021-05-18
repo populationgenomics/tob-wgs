46c91605b8a50107a0dd7caff29153d4cef00fae
pdiakumis <peterdiakumis@gmail.com>
2021-05-19 00:15:23 +1000

    test micromamba

--- reports/concordance/run_concordance.R
+++ reports/concordance/run_concordance.R
@@ -12,7 +12,12 @@ gcs_indir <- "gs://cpg-tob-wgs-snpchipdata/data"
 snpchipid2tobid_excel <- glue("{gcs_indir}/OneK1K_sample_IDs_2021-Apr-15.xlsx")
 snpchip_vcf_raw <- glue("{gcs_indir}/onek1k_pre_imputation_genotypes.vcf.gz")
 
-system(glue("gsutil cp {snpchipid2tobid_excel} {snpchip_vcf_raw} ."))
-cat("Listing files in current working directory:")
-list.files()
+# copy VCF + spreadsheet into container
+#system(glue("gsutil cp {snpchipid2tobid_excel} {snpchip_vcf_raw} ."))
+
+# get snpchip sample names from VCF
+
+cat("Testing micromamba")
+system("micromamba install bcftools -c bioconda -c conda-forge")
+system("bcftools --help")
 cat(glue("[{date()}] Finished!!!"))
