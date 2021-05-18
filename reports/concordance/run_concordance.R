0ed9ef991ff916c88b04e734e47ec6e1bdef9f2f
pdiakumis <peterdiakumis@gmail.com>
2021-05-18 23:41:02 +1000

    test data copying

--- reports/concordance/run_concordance.R
+++ reports/concordance/run_concordance.R
@@ -3,17 +3,16 @@
 require(tidyverse)
 require(glue)
 
+# authorise gcp service account
+gcp_auth_sa <- "gcloud -q auth activate-service-account --key-file=/gsa-key/key.json"
+system(gcp_auth_sa)
+
 gcs_outdir <- Sys.getenv("OUTPUT")
-mtcars_tsv <- "mtcars.tsv"
-mtcars_plot <- "mtcars_disp_vs_hp.png"
-d <- mtcars
-readr::write_tsv(d, file = mtcars_tsv)
+gcs_indir <- "gs://cpg-tob-wgs-snpchipdata/data"
+snpchipid2tobid_excel <- glue("{gcs_indir}/OneK1K_sample_IDs_2021-Apr-15.xlsx")
+snpchip_vcf_raw <- glue("{gcs_indir}/onek1k_pre_imputation_genotypes.vcf.gz")
 
-p <- d %>%
-  ggplot(aes(x = disp, y = hp)) +
-  geom_point()
-ggsave(mtcars_plot, p)
-
-system("gcloud -q auth activate-service-account --key-file=/gsa-key/key.json")
-system(glue("gsutil cp {mtcars_tsv} {mtcars_plot} {gcs_outdir}"))
-cat("That's all folks!!!")
+system(glue("gsutil cp {snpchipid2tobid_excel} {snpchip_vcf_raw} ."))
+cat("Listing files in current working directory:")
+list.files()
+cat(glue("[{date()}] Finished!!!"))
