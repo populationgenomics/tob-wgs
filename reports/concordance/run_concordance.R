ad695c11da8dd3f393215837e4b72c44ca2bdfde
pdiakumis <peterdiakumis@gmail.com>
2021-05-18 19:53:15 +1000

    testr

--- reports/concordance/run_concordance.R
+++ reports/concordance/run_concordance.R
@@ -4,9 +4,16 @@ require(tidyverse)
 require(glue)
 
 gcs_outdir <- Sys.getenv("OUTPUT")
-container_output <- "mtcars.tsv"
-x <- mtcars
-readr::write_tsv(x, file = container_output)
+mtcars_tsv <- "mtcars.tsv"
+mtcars_plot <- "mtcars_disp_vs_hp.png"
+d <- mtcars
+readr::write_tsv(d, file = container_output)
+
+p <- d %>%
+  ggplot(aes(x = disp, y = hp)) +
+  geom_point()
+ggsave(mtcars_plot, p)
+
 system("gcloud -q auth activate-service-account --key-file=/gsa-key/key.json")
-system(glue("gsutil cp {container_output} {gcs_outdir}"))
-head(x)
+system(glue("gsutil cp {mtcars_tsv} {mtcars_plot} {gcs_outdir}"))
+cat("That's all folks!!!")
