9685d4c14063e2a735d78255e8226d34e4d05353
pdiakumis <peterdiakumis@gmail.com>
2021-05-20 20:35:51 +1000

    transfer excel map to main bucket

--- reports/concordance/transfer_excel_map.sh
+++ reports/concordance/transfer_excel_map.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-# Transfer SNPchip ID-to-TOB ID Excel file into temporary bucket
-# gcloud -q auth activate-service-account --key-file=/gsa-key/key.json"
-
+# Transfer SNPchip ID-to-TOB ID Excel file into main bucket
 INPUT_EXCEL="gs://cpg-tob-wgs-snpchipdata/data/OneK1K_sample_IDs_2021-Apr-15.xlsx"
 
-if [[ "${OUTPUT}" =~ "gs://cpg-tob-wgs-temporary/peterd-snpchip" ]]; then
+if [[ "${OUTPUT}" =~ "gs://cpg-tob-wgs-main/snpchip/v1" ]]; then
     gsutil cp "${INPUT_EXCEL}" "${OUTPUT}"
 fi
