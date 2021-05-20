762e638333aa876833ab674848bba495dae8da2e
pdiakumis <peterdiakumis@gmail.com>
2021-05-20 19:45:45 +1000

    cleanup

--- reports/concordance/rename_snpchip_samples.R
+++ reports/concordance/rename_snpchip_samples.R
@@ -1,5 +1,13 @@
 #!/usr/bin/env Rscript
 
+# Rename SNPchip VCF sample names based on a TSV map.
+#
+# Input:
+#  - VCF with SNPchip genotypes
+#  - Excel file with mapping between SNPchip ID and TOB ID
+# Output:
+#  - VCF with samples renamed
+
 require(dplyr, include.only = c("mutate", "select", "%>%"))
 require(glue, include.only = "glue")
 require(readr, include.only = "write_tsv")
@@ -11,17 +19,17 @@ require(tibble, include.only = "as_tibble_col")
 gcp_auth_sa <- "gcloud -q auth activate-service-account --key-file=/gsa-key/key.json"
 system(gcp_auth_sa)
 
-cat("Installing bcftools\n")
+cat(glue("[{date()}] Installing bcftools.\n"))
 system("micromamba install --name base -c bioconda -c conda-forge bcftools")
 
-gcs_outdir <- Sys.getenv("OUTPUT")
+# set up inputs
 gcs_indir <- "gs://cpg-tob-wgs-snpchipdata/data"
-snpchipid2tobid_excel_gcs <- glue("{gcs_indir}/OneK1K_sample_IDs_2021-Apr-15.xlsx")
 snpchip_vcf_raw_gcs <- glue("{gcs_indir}/onek1k_pre_imputation_genotypes.vcf.gz")
-snpchipid2tobid_excel <- basename(snpchipid2tobid_excel_gcs)
 snpchip_vcf_raw <- basename(snpchip_vcf_raw_gcs)
+snpchipid2tobid_excel_gcs <- glue("{gcs_indir}/OneK1K_sample_IDs_2021-Apr-15.xlsx")
+snpchipid2tobid_excel <- basename(snpchipid2tobid_excel_gcs)
 
-# copy VCF + spreadsheet into container
+# copy inputs to container
 system(glue("gsutil cp {snpchipid2tobid_excel_gcs} {snpchip_vcf_raw_gcs} ."))
 
 # 1,034 samples in SNPchip VCF
@@ -29,14 +37,15 @@ chip_sample_nms <-
   system(glue("bcftools query -l {snpchip_vcf_raw}"), intern = TRUE) %>%
   as_tibble_col("sample")
 
-# 983 samples in Excel spreadsheet
+# 983 samples in Excel file
 chipid2tobid <-
   snpchipid2tobid_excel %>%
   read_excel(skip = 1) %>%
-  mutate(tob_id_final = glue("TOB{TOB_ID}")) %>%
-  select(sample = PERSON, tob_id = tob_id_final)
+  mutate(tob_id = glue("TOB{TOB_ID}")) %>%
+  select(sample = PERSON, tob_id)
 
-# TRUE - just write the map with 983 samples to use
+# all SNPchip IDs in the Excel file correspond to SNPchip IDs
+# in the VCF - just write the map with 983 samples to use
 # for VCF reheading. The 51 SNPchip samples without
 # a TOB ID will stay with their SNPchip IDs.
 all(chipid2tobid$sample %in% chip_sample_nms$sample)
@@ -48,13 +57,14 @@ chipid2tobid[ord, ] %>%
   write_tsv(file = chipid2tobid_tsv, col_names = FALSE)
 
 # rename VCF samples to TOB IDs according to map
-vcf_rehead <- "0-rehead.vcf.bgz"
+snpchip_vcf_rehead <- "snpchip_vcf_rehead.vcf.bgz"
 system(glue("bcftools reheader ",
             "-s {chipid2tobid_tsv} ",
-            "-o {vcf_rehead} ",
+            "-o {snpchip_vcf_rehead} ",
             "{snpchip_vcf_raw}"))
 
-# move data to bucket
-system(glue("gsutil cp {vcf_rehead} {gcs_outdir}"))
+# copy output to bucket
+gcs_outdir <- Sys.getenv("OUTPUT")
+system(glue("gsutil cp {snpchip_vcf_rehead} {gcs_outdir}"))
 
-cat(glue("[{date()}] Finished!!!"))
+cat(glue("[{date()}] Finished!!!\n"))
