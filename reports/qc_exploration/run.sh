eaa1bd01fdf3bbf8d8f94c047d62e5940d3c4adc
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-05-22 16:58:25 +1000

    Fix

--- reports/qc_exploration/run.sh
+++ reports/qc_exploration/run.sh
@@ -7,4 +7,4 @@ test -f age.csv || gsutil cp gs://cpg-tob-wgs-analysis/age.csv age.csv
 
 Rscript -e "rmarkdown::render('qc_exploration.Rmd', output_file='qc_exploration.html', params=list(gender_tsv='gender.tsv', age_csv='age.csv', qc_csv='qc.csv', bucket_suffix='test'))"
 
-gsutil cp qc_exploration.html $OUTPUT
+gsutil cp qc_exploration.html $OUTPUT/qc_exploration.html
