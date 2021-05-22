ed0a0d48bb2c9fc74b05247269337eafcc42ff5b
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-05-22 14:38:53 +1000

    Add runner for QC exploration

---
+++ reports/qc_exploration/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# gstuil cp gs://cpg-tob-wgs-main/gvcf/batch2/R_210315_BINKAN1_1K1KDNA_M003.csv data.csv
+gsutil cp gs://cpg-tob-wgs-test/gvcf/batch0/R_210315_BINKAN1_1K1KDNA_M001.csv qc.csv
+gsutil cp gs://cpg-tob-wgs-analysis/gender.tsv gender.tsv
+gsutil cp gs://cpg-tob-wgs-analysis/age.csv age.csv
+
+Rscript -e "rmarkdown::render('qc_exploration.Rmd', \
+output_file='qc_exploration.html', \
+params=list( \
+gender_tsv='gender.tsv' \
+age_csv='age.csv' \
+qc_csv='qc.csv', \
+bucket_suffix='test'))" ; \
+
+gsutil cp qc_exploration.html $OUTPUT
