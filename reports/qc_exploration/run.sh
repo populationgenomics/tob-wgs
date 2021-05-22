d4e3c8adf0fbe55eb2038770e27362138fdc0705
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-05-22 16:56:43 +1000

    Update run.sh

--- reports/qc_exploration/run.sh
+++ reports/qc_exploration/run.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# gstuil cp gs://cpg-tob-wgs-main/gvcf/batch2/R_210315_BINKAN1_1K1KDNA_M003.csv data.csv
-gsutil cp gs://cpg-tob-wgs-test/gvcf/batch0/R_210315_BINKAN1_1K1KDNA_M001.csv qc.csv
-gsutil cp gs://cpg-tob-wgs-analysis/gender.tsv gender.tsv
-gsutil cp gs://cpg-tob-wgs-analysis/age.csv age.csv
+test -f qc.csv || gsutil cp gs://cpg-tob-wgs-main/gvcf/batch2/R_210315_BINKAN1_1K1KDNA_M003.csv qc.csv
+test -f gender.tsv || gsutil cp gs://cpg-tob-wgs-analysis/gender.tsv gender.tsv
+test -f age.csv || gsutil cp gs://cpg-tob-wgs-analysis/age.csv age.csv
 
-Rscript -e "rmarkdown::render('qc_exploration.Rmd', \
-output_file='qc_exploration.html', \
-params=list( \
-gender_tsv='gender.tsv' \
-age_csv='age.csv' \
-qc_csv='qc.csv', \
-bucket_suffix='test'))" ; \
+Rscript -e "rmarkdown::render('qc_exploration.Rmd', output_file='qc_exploration.html', params=list(gender_tsv='gender.tsv', age_csv='age.csv', qc_csv='qc.csv', bucket_suffix='test'))"
 
 gsutil cp qc_exploration.html $OUTPUT
