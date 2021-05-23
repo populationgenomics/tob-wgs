b18ffc18816124c88be7caff7300fa2500b1bd0d
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-05-23 23:54:23 +1000

    Install extra packages in the script

--- reports/qc_exploration/run_test.sh
+++ reports/qc_exploration/run_test.sh
@@ -4,6 +4,7 @@ test -f qc.csv || gsutil cp gs://cpg-tob-wgs-test/gvcf/batch0/R_210315_BINKAN1_1
 test -f gender.tsv || gsutil cp gs://cpg-tob-wgs-test/gender.tsv gender.tsv
 test -f age.csv || gsutil cp gs://cpg-tob-wgs-test/age.csv age.csv
 
+mamba install -y -c conda-forge r-reactable r-ggrepel r-sessioninfo r-gargle r-here r-assertthat r-dt r-googlecloudstorager
 Rscript -e "rmarkdown::render('qc_exploration.Rmd', output_file='qc_exploration.html', params=list(gender_tsv='gender.tsv', age_csv='age.csv', qc_csv='qc.csv', bucket_suffix='test'))"
 
 gsutil cp qc_exploration.html $OUTPUT/qc_exploration.html
