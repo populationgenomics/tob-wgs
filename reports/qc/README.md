3c973b8bf7668a68ade30f6c5ccd897c23ee007c
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-05-24 01:51:57 +1000

    Change output-dir to temporary; set web in the script

--- reports/qc/README.md
+++ reports/qc/README.md
@@ -3,5 +3,5 @@
 Generate a report and put it into the web bucket. Will make a test report into the temporary bucket first.
 
 ```sh
-analysis-runner --dataset tob-wgs --access-level standard --output-dir "gs://cpg-tob-wgs-web/qc" --description "QC report" run.sh --prod --batch 2
+analysis-runner --dataset tob-wgs --access-level standard --output-dir "gs://cpg-tob-wgs-temporary/qc_report" --description "QC report" run.sh --prod --batch 2
 ```
--- reports/qc/run.sh
+++ reports/qc/run.sh
@@ -23,19 +23,20 @@ function run() {
 	local batch=$1
 	local analysis_suf=$2
 	local main_suf=$3
+	local web_suf=$4
 
 	test -f qc.csv || gsutil cp "gs://cpg-tob-wgs-main/gvcf/batch${batch}/*.csv" qc.csv
 	test -f gender.tsv || gsutil cp gs://cpg-tob-wgs-${analysis_suf}/gender.tsv gender.tsv
 	test -f age.csv || gsutil cp gs://cpg-tob-wgs-${analysis_suf}/age.csv age.csv
 	Rscript -e "rmarkdown::render('qc.Rmd', output_file='qc.html', params=list(gender_tsv='gender.tsv', age_csv='age.csv', qc_csv='qc.csv', bucket_suffix='${main_suf}'))"
-	gsutil cp qc.html $OUTPUT/qc.html
+	gsutil cp qc.html gs://cpg-tob-wgs-${web_suf}/qc/qc.html
 }
 
 # Run test first
-run 0 test test
+run 0 test test temporary
 
 # Run on full data in a standard access level
 if [ $PROD -eq YES ]
 then
-	run $BATCH analysis main
+	run $BATCH analysis main web
 fi
