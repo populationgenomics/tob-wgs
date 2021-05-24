11ba934029d09c7cf02a63ed3e8bf813a3428e48
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-05-24 12:07:49 +1000

    Fix to make sure the prod html uploaded to web

--- reports/qc/test.sh
+++ reports/qc/test.sh
@@ -5,15 +5,16 @@ function run() {
 	local analysis_suf=$2
 	local main_suf=$3
 	local web_suf=$4
+	local dir=work/${analysis_suf}
 
-	test -f work/gender.tsv || gsutil cp gs://cpg-tob-wgs-${analysis_suf}/gender.tsv work/gender.tsv
-	test -f work/age.csv || gsutil cp gs://cpg-tob-wgs-${analysis_suf}/age.csv work/age.csv
-	test -f work/qc.csv || gsutil cp "gs://cpg-tob-wgs-${main_suf}/gvcf/batch${batch}/*.csv" work/qc.csv
+	test -f ${dir}/gender.tsv || gsutil cp gs://cpg-tob-wgs-${analysis_suf}/gender.tsv ${dir}/gender.tsv
+	test -f ${dir}/age.csv || gsutil cp gs://cpg-tob-wgs-${analysis_suf}/age.csv ${dir}/age.csv
+	test -f ${dir}/qc.csv || gsutil cp "gs://cpg-tob-wgs-${main_suf}/gvcf/batch${batch}/*.csv" ${dir}/qc.csv
 	R --vanilla <<code
 rmarkdown::render('qc.Rmd', output_file='qc.html', params=list(\
-gender_tsv='work/gender.tsv', \
-age_csv='work/age.csv', \
-qc_csv='work/qc.csv', \
+gender_tsv='${dir}/gender.tsv', \
+age_csv='${dir}/age.csv', \
+qc_csv='${dir}/qc.csv', \
 gvcf_bucket_suffix='${main_suf}'\
 ))
 code
