7fa6401a61fc5ebf22e8bda2884fea12a9d8bbbf
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-05-24 14:57:41 +1000

    Add meta.tsv metrics from sample_qc

--- reports/qc/script-for-analysis-runner.sh
+++ reports/qc/script-for-analysis-runner.sh
@@ -27,16 +27,20 @@ function run() {
 	local analysis_suf=$2
 	local main_suf=$3
 	local web_suf=$4
+	local joint_calling_run_version=$5
+
 	local dir=work/${analysis_suf}
 
 	test -f ${dir}/gender.tsv || gsutil cp gs://cpg-tob-wgs-${analysis_suf}/gender.tsv ${dir}/gender.tsv
 	test -f ${dir}/age.csv || gsutil cp gs://cpg-tob-wgs-${analysis_suf}/age.csv ${dir}/age.csv
 	test -f ${dir}/qc.csv || gsutil cp "gs://cpg-tob-wgs-${main_suf}/gvcf/batch${batch}/*.csv" ${dir}/qc.csv
+	test -f ${dir}/meta.tsv || gsutil cp gs://cpg-tob-wgs-temporary/joint-calling/${joint_calling_run_version}/sample_qc/meta.tsv ${dir}/meta.tsv
 	R --vanilla <<code
 rmarkdown::render('qc.Rmd', output_file='qc.html', params=list(\
 gender_tsv='${dir}/gender.tsv', \
 age_csv='${dir}/age.csv', \
 qc_csv='${dir}/qc.csv', \
+meta_tsv='${dir}/meta.tsv', \
 gvcf_bucket_suffix='${main_suf}'\
 ))
 code
@@ -44,10 +48,10 @@ code
 }
 
 # Run test first
-run 0 test test temporary
+run 0 test test temporary test-v1
 
 # Run on full data in a standard access level
 if [[ $PROD = "YES" ]]
 then
-	run $BATCH analysis main web
+	run $BATCH analysis main web v1
 fi
--- reports/qc/test.sh
+++ reports/qc/test.sh
@@ -5,16 +5,20 @@ function run() {
 	local analysis_suf=$2
 	local main_suf=$3
 	local web_suf=$4
+	local joint_calling_run_version=$5
+
 	local dir=work/${analysis_suf}
 
 	test -f ${dir}/gender.tsv || gsutil cp gs://cpg-tob-wgs-${analysis_suf}/gender.tsv ${dir}/gender.tsv
 	test -f ${dir}/age.csv || gsutil cp gs://cpg-tob-wgs-${analysis_suf}/age.csv ${dir}/age.csv
 	test -f ${dir}/qc.csv || gsutil cp "gs://cpg-tob-wgs-${main_suf}/gvcf/batch${batch}/*.csv" ${dir}/qc.csv
+	test -f ${dir}/meta.tsv || gsutil cp gs://cpg-tob-wgs-temporary/joint-calling/${joint_calling_run_version}/sample_qc/meta.tsv ${dir}/meta.tsv
 	R --vanilla <<code
 rmarkdown::render('qc.Rmd', output_file='qc.html', params=list(\
 gender_tsv='${dir}/gender.tsv', \
 age_csv='${dir}/age.csv', \
 qc_csv='${dir}/qc.csv', \
+meta_tsv='${dir}/meta.tsv', \
 gvcf_bucket_suffix='${main_suf}'\
 ))
 code
@@ -22,4 +26,4 @@ code
 }
 
 # Run test first
-run 0 test test temporary
+run 0 test test temporary test-v0
