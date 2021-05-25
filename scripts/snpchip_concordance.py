69c4238b40ae84aa097f93abd68d1c59a252662f
pdiakumis <peterdiakumis@gmail.com>
2021-05-25 15:04:17 +1000

    liftover snpchip to GRCh38

--- scripts/snpchip_concordance.py
+++ scripts/snpchip_concordance.py
@@ -1,55 +1,33 @@
 """Concordance between SNPchip genotypes and WGS variant calls"""
 
 import os
-import subprocess
 import hail as hl
 
-# Rename sample columns that correspond to TOB IDs (51 columns do not have a TOB ID)
-DATA_DIR = os.path.join('nogit/data/snpchip')
-SNPCHIP_VCF_RAW = os.path.join(
-    f'{DATA_DIR}/raw/onek1k_pre_imputation_genotypes_2021-Mar-17.vcf.bgz'
-)
-SNPCHIP_VCF_REHEAD = os.path.join(f'{DATA_DIR}/processed/hail/0-rehead.vcf.bgz')
-SNPCHIP_ID2TOBID = os.path.join(
-    f'{DATA_DIR}/processed/sample2tobid_bcftools_reheader_2021-May-17.tsv'
-)
+output = os.getenv('OUTPUT')
+assert output and output.startswith('gs://cpg-tob-wgs-analysis/snpchip')
 
-subprocess.run(
-    [
-        'bcftools',
-        'reheader',
-        '-s',
-        SNPCHIP_ID2TOBID,
-        SNPCHIP_VCF_RAW,
-        '-o',
-        SNPCHIP_VCF_REHEAD,
-    ],
-    check=False,
-)
+INPUT_VCF = 'gs://cpg-tob-wgs-analysis/snpchip/snpchip_vcf_rehead.vcf.bgz'
+OUTPUT_MT = f'{output}/snpchip_hg38.mt'
 
-# Explore VCF in Hail
 hl.init()
 
-SNPCHIP_MT = os.path.join(f'{DATA_DIR}/processed/hail/0-rehead.mt')
-
-hl.import_vcf(SNPCHIP_VCF_REHEAD).write(SNPCHIP_MT)
-mt = hl.read_matrix_table(SNPCHIP_MT)
+mt = hl.import_vcf(INPUT_VCF)
 
 # Explore MT
-mt.count()  # 483,482 x 1,034
-mt.rows().show()  # show CHR:POS, [REF,ALT], ID, QUAL, FILTER, INFO
-mt.s.show(5)  # show first 5 samples
-mt.entry.show(5)  # show first 5 GTs
+mt.count()
+mt.rows().show()
+mt.s.show(5)
+mt.entry.show(5)
 
 
 def liftover(x):
     """
-    Liftover from GRCh37 to GRCh38
+    Liftover matrix table x from GRCh37 to GRCh38
     """
-    ref37 = hl.get_reference('GRCh37')
-    ref38 = hl.get_reference('GRCh38')
-    ref37.add_liftover(
-        os.path.join(f'{DATA_DIR}/reference/grch37_to_grch38.over.chain.gz'), ref38
+    rg37 = hl.get_reference('GRCh37')
+    rg38 = hl.get_reference('GRCh38')
+    rg37.add_liftover(
+        'gs://hail-common/references/grch37_to_grch38.over.chain.gz', rg38
     )
     x = x.annotate_rows(new_locus=hl.liftover(x.locus, 'GRCh38'))
     x = x.filter_rows(hl.is_defined(x.new_locus))
@@ -57,5 +35,9 @@ def liftover(x):
     return x
 
 
+# Liftover to GRCh38
 mt = liftover(mt)
 mt.rows().show()
+
+# Write lifted over MT to analysis bucket
+mt.write(OUTPUT_MT)
