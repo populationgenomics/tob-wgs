0087053d70d66c548277c190ff6fd22b94239826
pdiakumis <peterdiakumis@gmail.com>
2021-05-29 11:44:52 +1000

    separate data transfer scripts

--- scripts/data_transfer/move_pipeline_validation_gvcfs.sh
+++ scripts/data_transfer/move_pipeline_validation_gvcfs.sh
@@ -8,4 +8,3 @@ for sample in ERR1341796 HG01513 HG02238 NA12248 NA12878 NA20502 NA20826
 do
     gsutil -m mv "gs://cpg-tob-wgs-upload/$sample.*" $OUTPUT
 done
-
--- scripts/data_transfer/move_uploaded_scrna_seq_fastqs.sh
+++ scripts/data_transfer/move_uploaded_scrna_seq_fastqs.sh
@@ -7,4 +7,3 @@ gsutil mv gs://cpg-tob-wgs-upload/onek1k_scrna_seq_data_md5sums.txt gs://cpg-tob
 
 # Copy a subset of the data to the `test` bucket.
 gsutil -m cp -r gs://cpg-tob-wgs-main/scrna_seq/fastq/v0/OneK1K_scRNA_Sample1 gs://cpg-tob-wgs-main/scrna_seq/fastq/v0/OneK1K_scRNA_Sample2 gs://cpg-tob-wgs-test/scrna_seq/fastq/v0
-
