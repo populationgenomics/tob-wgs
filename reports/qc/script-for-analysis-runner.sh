0293624602b8799803de5ff8b5504dd81179206b
Vlad Savelyev <vladislav.sav@gmail.com>
2021-06-01 02:16:56 +1000

    Change bucket pointers (#36)
    * Fix buckets (see https://github.com/populationgenomics/analysis-runner/pull/117/files)
    
    * test-web bucket doesnt exist yet

--- reports/qc/script-for-analysis-runner.sh
+++ reports/qc/script-for-analysis-runner.sh
@@ -31,17 +31,14 @@ function run() {
 	local batch=$1
 	local analysis_suf=$2
 	local main_suf=$3
-	local web_suf=$4
-	local joint_calling_run_version=$5
-	local is_test=$6
+	local joint_calling_run_version=$4
+	local is_test=$5
 
 	local dir=work/${analysis_suf}
 	local test="FALSE"
-	local out_version="${joint_calling_run_version}"
 	local meta_csv_path="gs://cpg-tob-wgs-temporary/joint-calling/${joint_calling_run_version}/sample_qc/meta.tsv"
 	if [ $is_test -eq 1 ]; then
 		test="TRUE"
-		out_version="test-${out_version}"
 		meta_csv_path="gs://cpg-tob-wgs-test/joint-calling/test-${joint_calling_run_version}/sample_qc/meta.tsv"
 	fi
 
@@ -59,14 +56,13 @@ meta_tsv='${dir}/meta.tsv', \
 gvcf_bucket_suffix='${main_suf}'\
 ))
 code
-	gsutil cp qc.html gs://cpg-tob-wgs-web/qc/qc-${out_version}.html
+	gsutil cp qc.html gs://cpg-tob-wgs-${main_suf}-web/qc/qc-${joint_calling_run_version}.html
 }
-
-# Run test first
-run 1 test test temporary $VERSION 1
 
 # Run on full data in a standard access level
 if [[ $PROD = "YES" ]]
 then
-	run $BATCH analysis main web $VERSION 0
+	run $BATCH analysis main $VERSION 0
+else
+	run 1 test test $VERSION 1
 fi
