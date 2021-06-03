3890d6af8d1e7f0bf68c0dd30dca46c14eb35780
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-06-03 10:40:02 +1000

    Replace with bash script

---
+++ scripts/data_transfer/copy_joint_calling_metadata.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+VERSION=$1
+
+if [[ -z "$VERSION" ]]; then
+    echo "Provide joint-calling run version as a first argument" 1>&2
+    exit 1
+fi
+
+gsutil cp \
+	gs://cpg-tob-wgs-main/joint-calling/${VERSION}/meta.tsv \
+	gs://cpg-tob-wgs-main-metadata/joint-calling/${VERSION}/meta.tsv
