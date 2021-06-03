83f6250e54946dea484366940ae3de1ee4dc3953
Vlad Savelyev <vladislav.sav@gmail.com>
2021-06-03 12:09:22 +1000

    Add script to copy metadata to test reporting (#44)
    * Add script to copy metadata for reporting
    
    * use metadata path
    
    * Copy only joint-calling meta.tsv. Remove trailing-whitespace inspection
    
    * Correct to main-metadata
    
    * Update scripts/data_transfer/copy_test_data_for_reports.py
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
    
    * Replace with bash script
    
    * Fix shebang
    
    * Wording
    
    * Use OUTPUT
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>

---
+++ scripts/data_transfer/copy_joint_calling_metadata.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+VERSION=$1
+
+if [[ -z "$VERSION" ]]; then
+    echo "Provide a version tag of a joint-calling run as a first argument" 1>&2
+    exit 1
+fi
+
+target_bucket=gs://cpg-tob-wgs-main-metadata/joint-calling
+if [[ ${OUTPUT} != ${target_bucket}* ]]; then
+	echo "The analysis runner output directory must start with ${target_bucket}"
+	exit 1
+fi	
+
+gsutil cp \
+	gs://cpg-tob-wgs-main/joint-calling/${VERSION}/meta.tsv \
+	${target_bucket}/${VERSION}/meta.tsv
