16875fed74ec41d9eaddb69e3cb4499b1c403b18
pdiakumis <peterdiakumis@gmail.com>
2021-06-04 03:25:43 +1000

    use service backend

--- reports/concordance/batch_concordance.py
+++ reports/concordance/batch_concordance.py
@@ -1,32 +1,40 @@
+#!/usr/bin/env python3
+
 """Concordance between SNPchip and WGS samples"""
 
+import os
 import hailtop.batch as hb
 
 
-def concordance(b, snpmt, wgsmt):
+def concordance(batch, snpmt, wgsmt):
     """
     Concordance between SNPchip and WGS samples
     """
-    c = b.new_job(name='run-concordance')
-    c.image('pdiakumis/concordance:0.1')
-    c.command(
+    conc = batch.new_job(name='run-concordance')
+    conc.image('pdiakumis/concordance:0.1')
+    conc.command(
         f"""
         set -e
         concordance \
           --snp {snpmt} \
           --wgs {wgsmt} \
-          --html {c.ofile}
+          --html {conc.ofile}
         """
     )
-    return c
+    return conc
 
 
 if __name__ == '__main__':
-    batch = hb.Batch(name='concordance')
+    service_backend = hb.ServiceBackend(
+        billing_project=os.getenv('HAIL_BILLING_PROJECT'),
+        bucket=os.getenv('HAIL_BUCKET'),
+    )
+    b = hb.Batch(backend=service_backend, name='concordance')
+
     BUCKET = 'gs://cpg-tob-wgs-test'
-    snp = batch.read_input(f'{BUCKET}/snpchip/v1/snpchip_grch38.mt')
-    wgs = batch.read_input(f'{BUCKET}/mt/test-v1-raw.mt')
-    html = f'concordance_snpchip_with_wgs.html'
-    concordance = concordance(batch, snp, wgs)
-    batch.write_output(concordance.ofile, f'{BUCKET}-web/concordance/v1/{html}')
-    batch.run(dry_run=False)
+    snp = b.read_input(f'{BUCKET}/snpchip/v1/snpchip_grch38.mt')
+    wgs = b.read_input(f'{BUCKET}/mt/test-v1-raw.mt')
+    HTML = 'concordance_snpchip_with_wgs.html'
+    concordance = concordance(b, snp, wgs)
+    b.write_output(concordance.ofile, f'{BUCKET}-web/concordance/v1/{HTML}')
+    b.run(dry_run=False)
