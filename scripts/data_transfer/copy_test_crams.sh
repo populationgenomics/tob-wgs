3e22481f0552de1a0b6f800d98842526f28b56a5
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2021-06-08 09:19:24 +1000

    Use $OUTPUT instead of hardcoded destination

--- scripts/data_transfer/copy_test_crams.sh
+++ scripts/data_transfer/copy_test_crams.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-gsutil -m cp "gs://cpg-tob-wgs-archive/cram/batch1/TOB152[0-2].cram*" gs://cpg-tob-wgs-test/cram/batch1/
+gsutil -m cp "gs://cpg-tob-wgs-archive/cram/batch1/TOB152[0-2].cram*" $OUTPUT
 
