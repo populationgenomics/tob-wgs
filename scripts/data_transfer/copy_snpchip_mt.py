ddc3678a1b59cc0f3ff0bc50c8ce45a0070a7ad0
pdiakumis <peterdiakumis@gmail.com>
2021-06-15 16:05:57 +1000

    copy snpchip mt to main bucket

---
+++ scripts/data_transfer/copy_snpchip_mt.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Copies GRCh38 SNPchip MatrixTable to the tob-wgs main bucket.
+"""
+
+import os
+import subprocess
+
+output = os.getenv('OUTPUT')
+assert output and output.startswith('gs://cpg-tob-wgs-main/snpchip/v1')
+
+INPUT_MT = 'gs://cpg-tob-wgs-test/snpchip/v1/snpchip_grch38.mt'
+
+subprocess.run(['gsutil', '-m', 'cp', '-r', INPUT_MT, output], check=False)
