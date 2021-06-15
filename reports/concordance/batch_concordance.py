f593837d504a47e1ea812cc690a15e470fa0e8af
Peter Diakumis <peterdiakumis@gmail.com>
2021-06-15 18:53:03 +1000

    Hail concordance (#35)
    * calculate concordance for test samples
    
    * show concordance results
    
    * extend concordance table
    
    * cleanup
    
    * add concordance cli script
    
    * fix table
    
    * add Dockerfile + conda env
    
    * cleanup and simplify inputs
    
    * add batch concordance script
    
    * use service backend
    
    * try trailing forward slash
    
    * unphase GTs; change concordance formula
    
    * add RES argument
    
    * add RES argument
    
    * wet run
    
    * bloody r
    
    * bump v0.1.1
    
    * add gcs connector
    
    * bump v0.1.2
    
    * add cpu, mem and storage
    
    * pandoc: avoid automatic file extension
    
    * bump v0.1.3
    
    * try 8 CPUs
    
    * bump v0.1.4
    
    * wgs mt: densify, lgt_to_gt, and split_multi_hts
    
    * increase cpu
    
    * bump v0.1.5
    
    * remove terrible py chunk colour
    
    * chr22 -> wg
    
    * bump v0.1.6
    
    * add README
    
    * write per-site concordance table
    
    * bump v0.1.7
    
    * fix suffix
    
    * export mt subsets
    
    * bump v0.1.8
    
    * overwrite mt
    
    * bump v0.1.9
    
    * try highmem vm
    
    * bump v0.1.10
    
    * subset to chr22
    
    * bump v0.1.11
    
    * bump v0.1.12
    
    * clarify left/right; bump cores
    
    * bump v0.1.13
    
    * reduce cpu

---
+++ reports/concordance/batch_concordance.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+"""Concordance between SNPchip and WGS samples"""
+
+import os
+import hailtop.batch as hb
+
+
+def concordance(batch, snpmt, wgsmt, cpu):
+    """
+    Concordance between SNPchip and WGS samples
+    """
+    conc = batch.new_job(name='run-concordance')
+    conc.image('pdiakumis/concordance:0.1.13')
+    conc.cpu(cpu)
+    conc.memory('highmem')
+    conc.storage('100G')
+    conc.command(
+        f"""
+        set -e
+        concordance \
+          --snp {snpmt} \
+          --wgs {wgsmt} \
+          --res_samples {conc.res_samples_tsv} \
+          --html {conc.html} \
+          --cpu {cpu}
+        """
+    )
+    return conc
+
+
+if __name__ == '__main__':
+    service_backend = hb.ServiceBackend(
+        billing_project=os.getenv('HAIL_BILLING_PROJECT'),
+        bucket=os.getenv('HAIL_BUCKET'),
+    )
+    b = hb.Batch(backend=service_backend, name='concordance')
+
+    BUCKET = 'gs://cpg-tob-wgs-test'
+    SNP = f'{BUCKET}/snpchip/v1/snpchip_grch38.mt'
+    WGS = f'{BUCKET}/mt/test-v1-raw.mt'
+    CPU = 16
+    PREFIX = 'concordance_19samples_chr22'
+    HTML = f'{PREFIX}.html'
+    concordance = concordance(b, SNP, WGS, CPU)
+    b.write_output(concordance.html, f'{BUCKET}-web/concordance/v1/{HTML}')
+    b.write_output(
+        concordance.res_samples_tsv, f'{BUCKET}/concordance/v1/{PREFIX}_samples.tsv'
+    )
+    b.run(dry_run=False)
+    service_backend.close()
