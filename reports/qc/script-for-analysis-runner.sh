819e55e9deb4a4f8a48d797fe766b64980088927
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-06-18 00:39:34 +1000

    Add option to run from tmp

--- reports/qc/script-for-analysis-runner.sh
+++ reports/qc/script-for-analysis-runner.sh
@@ -15,6 +15,10 @@ case $key in
     PROD=YES
     shift # past argument
     ;;
+    --tmp)
+    TMP=YES
+    shift
+    ;;
     -b|--batch)
     BATCH="$2"
     shift # past argument
@@ -42,7 +46,12 @@ function run() {
 	test -f ${dir}/reported_sex.tsv || gsutil cp "gs://cpg-tob-wgs-main-metadata/reported_sex.tsv" ${dir}/reported_sex.tsv
 	test -f ${dir}/age.csv          || gsutil cp "gs://cpg-tob-wgs-main-metadata/age.csv" ${dir}/age.csv
 	test -f ${dir}/qc.csv           || gsutil cp "gs://cpg-tob-wgs-main-metadata/${batch}/*.csv" ${dir}/qc.csv
-	test -f ${dir}/meta.tsv         || gsutil cp "gs://cpg-tob-wgs-main-metadata/joint-calling/${joint_calling_run_version}/meta.tsv" ${dir}/meta.tsv
+	if [[ $TMP = "YES" ]]
+	then
+		test -f ${dir}/meta.tsv     || gsutil cp "gs://cpg-tob-wgs-test-tmp/joint-calling/${joint_calling_run_version}/meta.tsv" ${dir}/meta.tsv
+	else
+		test -f ${dir}/meta.tsv     || gsutil cp "gs://cpg-tob-wgs-main-metadata/joint-calling/${joint_calling_run_version}/meta.tsv" ${dir}/meta.tsv
+	fi
 	cat qc.Rmd | sed 's/r fig.width=[1-9]*, fig.height=[1-9]*/r fig.width=plot_width, fig.height=plot_height/g' > qc-for-html.Rmd
 	R --vanilla <<code
 rmarkdown::render('qc-for-html.Rmd', output_file='qc.html', params=list(\
