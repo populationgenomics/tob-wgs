00506bcbb2c1de05f90a68f30919534d79e7f5ef
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2021-07-01 16:36:20 +1000

    Copy 20 gvcfs. Remove old scripts

--- scripts/data_transfer/copy_test_gvcfs.py
+++ scripts/data_transfer/copy_test_gvcfs.py
@@ -5,25 +5,47 @@ import subprocess
 
 output = os.getenv('OUTPUT')
 assert output and output.startswith('gs://cpg-tob-wgs-test/')
-
-SUBSET_RE = 'TOB15[2-3]'
 QC_METADATA_FILE_NAME = 'R_210315_BINKAN1_1K1KDNA_M001.csv'
 
-subprocess.run(
-    [
-        'gsutil',
-        'cp',
-        f'gs://cpg-tob-wgs-main/gvcf/batch0/{SUBSET_RE}?.g.vcf.gz*',
-        'gs://cpg-tob-wgs-test/gvcf/batch0/',
-    ],
-    check=False,
-)
+SAMPLES = [
+    'TOB1520',
+    'TOB1521',
+    'TOB1522',
+    'TOB1523',
+    'TOB1524',
+    'TOB1525',
+    'TOB1526',
+    'TOB1527',
+    'TOB1528',
+    'TOB1529',
+    'TOB1530',
+    'TOB1531',
+    'TOB1532',
+    'TOB1533',
+    'TOB1534',
+    'TOB1535',
+    'TOB1536',
+    'TOB1537',
+    'TOB1538',
+    'TOB1640',
+]
+
+for sn in SAMPLES:
+    subprocess.run(
+        [
+            'gsutil',
+            'cp',
+            f'gs://cpg-tob-wgs-main/gvcf/batch1/{sn}.g.vcf.gz*',
+            'gs://cpg-tob-wgs-test/gvcf/batch1/',
+        ],
+        check=False,
+    )
 
 # Copy metadata subset.
 subprocess.run(
-    f'gsutil cat gs://cpg-tob-wgs-main/gvcf/batch0/{QC_METADATA_FILE_NAME} | '
-    f'grep -e "sample\\|{SUBSET_RE}" | '  # Also copy header.
-    f'gsutil cp - gs://cpg-tob-wgs-test/gvcf/batch0/{QC_METADATA_FILE_NAME}',
+    f'gsutil cat gs://cpg-tob-wgs-main-metadata/batch1/{QC_METADATA_FILE_NAME} | '
+    f'grep -P "sample|{"|".join(SAMPLES)}" | '  # Also copy header.
+    f'gsutil cp - gs://cpg-tob-wgs-test-metadata/batch1/{QC_METADATA_FILE_NAME}',
     shell=True,
     check=False,
 )
--- scripts/data_transfer/fix_metadata_batch_0_1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Overwrite previous metadata files, as a column had been swapped.
-gsutil mv gs://cpg-tob-wgs-upload/R_210315_BINKAN1_1K1KDNA_M001.csv gs://cpg-tob-wgs-main/gvcf/batch0
-gsutil mv gs://cpg-tob-wgs-upload/R_210315_BINKAN1_1K1KDNA_M002.csv gs://cpg-tob-wgs-main/gvcf/batch1
