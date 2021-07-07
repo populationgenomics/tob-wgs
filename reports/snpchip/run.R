ed81b3e30f438cc5850f630d6afdfaa324c920a4
pdiakumis <peterdiakumis@gmail.com>
2021-07-07 21:58:52 +1000

    check installed R pkgs

--- reports/snpchip/run.R
+++ reports/snpchip/run.R
@@ -1,56 +1,56 @@
 #!/usr/bin/env Rscript
 
+sort(unname(installed.packages()[,1]))
 # CLI for snpchip_ids.Rmd
 
-r_pkgs <- paste0("r-", c("argparser", "assertthat", "dt", "janitor", "glue", "sessioninfo"))
-cat(paste0("[",  date(), "] ", "Installing conda pkgs for report.\n"))
-system(paste("micromamba install --name base -c bioconda -c conda-forge", "bcftools", r_pkgs))
-
-spsm <- suppressPackageStartupMessages
-#spsm(require(argparser, include.only = c("parse_args", "arg_parser", "add_argument")))
-spsm(require(rmarkdown, include.only = "render"))
-spsm(require(glue, include.only = "glue"))
-spsm(require(dplyr, include.only = "if_else"))
-spsm(require(assertthat, include.only = "assert_that"))
-
-bucket_path <- function(path, bucket_category = NULL) {
-
-  dataset <- Sys.getenv("DATASET")
-  access_level <- Sys.getenv("ACCESS_LEVEL")
-  assert_that(nchar(dataset) > 0, nchar(access_level) > 0)
-
-  namespace <- if_else(access_level == "test", "test", "main")
-  if (is.null(bucket_category)) {
-    bucket_category <- namespace
-  } else if (!bucket_category %in% c("archive", "upload")) {
-    bucket_category <- glue("{namespace}-{bucket_category}")
-  }
-
-  return(file.path("gs:/", glue("cpg-{dataset}-{bucket_category}"), path))
-}
-
-output_path <- function(path_suffix, bucket_category = NULL) {
-  output <- Sys.getenv("OUTPUT")
-  assert_that(nchar(output) > 0)
-  return(bucket_path(file.path(output, path_suffix), bucket_category))
-}
-
-params_list <- list(
-  input = bucket_path("snpchip/v1")
-)
-html_suffix <- "snpchip_ids.html"
-html <- output_path(html_suffix, "web")
-
-cat("params_list:\n")
-print(params_list)
-cat("html:\n")
-print(html)
-
-cat(glue("[{as.character(Sys.time())}] START analysis!"))
-render(
-  input = "snpchip_ids.Rmd",
-  output_file = html_suffix,
-  params = params_list
-)
-system(glue("gsutil cp {html_suffix} {html}"))
-cat(glue("[{as.character(Sys.time())}] END analysis!"))
+#r_pkgs <- paste0("r-", c("assertthat", "dt", "janitor", "glue", "sessioninfo"))
+#cat(paste0("[",  date(), "] ", "Installing conda pkgs for report.\n"))
+#system(paste("micromamba install --name base -c bioconda -c conda-forge", "bcftools", r_pkgs))
+#
+#spsm <- suppressPackageStartupMessages
+#spsm(require(rmarkdown, include.only = "render"))
+#spsm(require(glue, include.only = "glue"))
+#spsm(require(dplyr, include.only = "if_else"))
+#spsm(require(assertthat, include.only = "assert_that"))
+#
+#bucket_path <- function(path, bucket_category = NULL) {
+#
+#  dataset <- Sys.getenv("DATASET")
+#  access_level <- Sys.getenv("ACCESS_LEVEL")
+#  assert_that(nchar(dataset) > 0, nchar(access_level) > 0)
+#
+#  namespace <- if_else(access_level == "test", "test", "main")
+#  if (is.null(bucket_category)) {
+#    bucket_category <- namespace
+#  } else if (!bucket_category %in% c("archive", "upload")) {
+#    bucket_category <- glue("{namespace}-{bucket_category}")
+#  }
+#
+#  return(file.path("gs:/", glue("cpg-{dataset}-{bucket_category}"), path))
+#}
+#
+#output_path <- function(path_suffix, bucket_category = NULL) {
+#  output <- Sys.getenv("OUTPUT")
+#  assert_that(nchar(output) > 0)
+#  return(bucket_path(file.path(output, path_suffix), bucket_category))
+#}
+#
+#params_list <- list(
+#  input = bucket_path("snpchip/v1")
+#)
+#html_suffix <- "snpchip_ids.html"
+#html <- output_path(html_suffix, "web")
+#
+#cat("params_list:\n")
+#print(params_list)
+#cat("html:\n")
+#print(html)
+#
+#cat(glue("[{as.character(Sys.time())}] START analysis!"))
+#render(
+#  input = "snpchip_ids.Rmd",
+#  output_file = html_suffix,
+#  params = params_list
+#)
+#system(glue("gsutil cp {html_suffix} {html}"))
+#cat(glue("[{as.character(Sys.time())}] END analysis!"))
