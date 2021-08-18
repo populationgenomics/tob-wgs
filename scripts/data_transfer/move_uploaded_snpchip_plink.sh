c8d04a510dd440078d06cf7b00694147da857754
pdiakumis <peterdiakumis@gmail.com>
2021-08-18 14:02:03 +1000

    use main-upload bucket

--- scripts/data_transfer/move_uploaded_snpchip_plink.sh
+++ scripts/data_transfer/move_uploaded_snpchip_plink.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-INPUT_PATH="gs://cpg-tob-wgs-upload/one1k1_genotyping_all_regions"
+INPUT_PATH="gs://cpg-tob-wgs-main-upload/one1k1_genotyping_all_regions"
 OUTPUT_PATH="snpchip/v1/plink"
 DESCRIPTION="OneK1K SNPchip data in Plink format, provided 2021-Jul-20 by GWCCG."
 
