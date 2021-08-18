76c597da64c43f05d6e3af30891266fe5519316c
pdiakumis <peterdiakumis@gmail.com>
2021-08-18 14:00:10 +1000

    skip copying to test

--- scripts/data_transfer/move_uploaded_snpchip_plink.sh
+++ scripts/data_transfer/move_uploaded_snpchip_plink.sh
@@ -6,10 +6,6 @@ INPUT_PATH="gs://cpg-tob-wgs-upload/one1k1_genotyping_all_regions"
 OUTPUT_PATH="snpchip/v1/plink"
 DESCRIPTION="OneK1K SNPchip data in Plink format, provided 2021-Jul-20 by GWCCG."
 
-# Copy files to 'test' for exploration with Hail.
-gsutil -m cp -r ${INPUT_PATH} gs://cpg-tob-wgs-test/${OUTPUT_PATH}
-echo ${DESCRIPTION} | gsutil cp - gs://cpg-tob-wgs-test/${OUTPUT_PATH}/README.txt
-
-# Then move to 'main'.
+# Move to 'main'.
 gsutil -m mv ${INPUT_PATH} gs://cpg-tob-wgs-main/${OUTPUT_PATH}
 echo ${DESCRIPTION} | gsutil cp - gs://cpg-tob-wgs-main/${OUTPUT_PATH}/README.txt
