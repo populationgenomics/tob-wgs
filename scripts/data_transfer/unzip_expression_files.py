06f5232527de73b0c9ee0e02592d58164cd8f4a5
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-02-21 17:00:52 +1100

    Unzip tar files for expression data (#79)
    * Unzip tar files for expression data
    
    * add in specific file locations of where to output files
    
    Files are output to `main` and also a subset is put into `test`
    
    * Remove unused --output parameter
    
    * Add @click.command() to make click options work
    
    * Remove flag to get rid of error 'tar: z: Cannot open: No such file or directory'
    
    * Add in driver image in order to recognise gsutil command
    
    * Only add in folder path relative to the VM
    
    * Remove unnecessary line of example code
    
    * Fix mv command to go to output folder, rather than file
    
    * Test moving all files in test bucket
    
    * Copy rather than move files to solve unfound files issue
    
    * Move from test bucket to main
    
    * Make dor to copy from into a variable
    
    * Change from test to main

---
+++ scripts/data_transfer/unzip_expression_files.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+"""Untar and unzip expression files"""
+
+import os
+import click
+import hailtop.batch as hb
+
+from analysis_runner.constants import GCLOUD_ACTIVATE_AUTH
+
+@click.command()
+@click.option(
+    '--file',
+    required=True,
+    help='File to unzip and untar',
+)
+def main(file: str):
+    """
+    Untar and unzip files
+    """
+    dataset = os.getenv('DATASET')
+    access_level = os.getenv('ACCESS_LEVEL')
+    backend = hb.ServiceBackend(
+        billing_project=dataset, bucket=f'cpg-{dataset}-{access_level}'
+    )
+    b = hb.Batch(name='untar-files', backend=backend)
+    tarfile = b.read_input(file)
+    dir_to_copy_from = os.path.basename(file).split('.')[0]
+    j = b.new_job('untar-files')
+    j.image(os.getenv('DRIVER_IMAGE'))
+    j.command(f'tar xvfz {tarfile}')
+    j.command(GCLOUD_ACTIVATE_AUTH)
+    j.command(f"""gsutil cp {dir_to_copy_from}/* gs://cpg-tob-wgs-main/scrna-seq/grch38_association_files/expression_files/""")
+    j.command(f"""gsutil mv {dir_to_copy_from}/B_intermediate_expression.tsv {dir_to_copy_from}/B_memory_expression.tsv {dir_to_copy_from}/B_naive_expression.tsv gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/expression_files/""")
+    b.run(wait=False)
+
+if __name__ == '__main__':
+    # pylint: disable=no-value-for-parameter
+    main()
