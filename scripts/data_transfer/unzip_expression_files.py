0dd74ea5b38577d60514abb528eb0ac2fc08b456
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-02-21 16:50:33 +1100

    Make dor to copy from into a variable

--- scripts/data_transfer/unzip_expression_files.py
+++ scripts/data_transfer/unzip_expression_files.py
@@ -25,12 +25,13 @@ def main(file: str):
     )
     b = hb.Batch(name='untar-files', backend=backend)
     tarfile = b.read_input(file)
+    dir_to_copy_from = os.path.basename(file).split('.')[0]
     j = b.new_job('untar-files')
     j.image(os.getenv('DRIVER_IMAGE'))
     j.command(f'tar xvfz {tarfile}')
     j.command(GCLOUD_ACTIVATE_AUTH)
-    j.command(f"""gsutil cp expression_220218/* gs://cpg-tob-wgs-main/scrna-seq/grch38_association_files/expression_files/""")
-    j.command(f"""gsutil mv expression_220218/B_intermediate_expression.tsv expression_220218/B_memory_expression.tsv expression_220218/B_naive_expression.tsv gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/expression_files/""")
+    j.command(f"""gsutil cp {dir_to_copy_from}/* gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/expression_files/""")
+    j.command(f"""gsutil mv {dir_to_copy_from}/B_intermediate_expression.tsv {dir_to_copy_from}/B_memory_expression.tsv {dir_to_copy_from}/B_naive_expression.tsv gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/expression_files/""")
     b.run(wait=False)
 
 if __name__ == '__main__':
