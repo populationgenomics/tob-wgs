476d79556fe93b5317b70cee0078bcb324172b4d
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-02-21 15:59:03 +1100

    Only add in folder path relative to the VM

--- scripts/data_transfer/unzip_expression_files.py
+++ scripts/data_transfer/unzip_expression_files.py
@@ -23,16 +23,16 @@ def main(file: str):
     )
     b = hb.Batch(name='untar-files', backend=backend)
     tarfile = b.read_input(file)
-    file_folder = file.split('.')[0]
     j = b.new_job('untar-files')
     j.image(os.getenv('DRIVER_IMAGE'))
     j.command(f'tar xvfz {tarfile}')
     # j.command(f"""gsutil mv -r {file_folder} gs://cpg-tob-wgs-main/scrna-seq/grch38_association_files/expression_files/""")
-    j.command(f"""gsutil mv -r {file_folder} gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/expression_files/B_intermediate_expression.tsv""")
-    j.command(f"""gsutil mv -r {file_folder} gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/expression_files/B_memory_expression.tsv""")
-    j.command(f"""gsutil mv -r {file_folder} gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/expression_files/B_naive_expression.tsv""")
+    j.command(f"""gsutil mv -r expression_220218 gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/expression_files/B_intermediate_expression.tsv""")
+    j.command(f"""gsutil mv -r expression_220218 gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/expression_files/B_memory_expression.tsv""")
+    j.command(f"""gsutil mv -r expression_220218 gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/expression_files/B_naive_expression.tsv""")
     b.run(wait=False)
 
+gsutil mv -r gs://cpg-tob-wgs-main-upload/expression_220218 gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/expression_files/B_naive_expression.tsv
 
 if __name__ == '__main__':
     # pylint: disable=no-value-for-parameter
