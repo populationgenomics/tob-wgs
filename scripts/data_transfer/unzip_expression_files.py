5c5e35bd442e78bfc3b952a1d4533b72d9ec4722
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-02-21 16:18:22 +1100

    Fix mv command to go to output folder, rather than file

--- scripts/data_transfer/unzip_expression_files.py
+++ scripts/data_transfer/unzip_expression_files.py
@@ -6,6 +6,8 @@ import os
 import click
 import hailtop.batch as hb
 
+from analysis_runner.constants import GCLOUD_ACTIVATE_AUTH
+
 @click.command()
 @click.option(
     '--file',
@@ -26,10 +28,9 @@ def main(file: str):
     j = b.new_job('untar-files')
     j.image(os.getenv('DRIVER_IMAGE'))
     j.command(f'tar xvfz {tarfile}')
+    j.command(GCLOUD_ACTIVATE_AUTH)
     # j.command(f"""gsutil mv -r {file_folder} gs://cpg-tob-wgs-main/scrna-seq/grch38_association_files/expression_files/""")
-    j.command(f"""gsutil mv -r expression_220218 gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/expression_files/B_intermediate_expression.tsv""")
-    j.command(f"""gsutil mv -r expression_220218 gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/expression_files/B_memory_expression.tsv""")
-    j.command(f"""gsutil mv -r expression_220218 gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/expression_files/B_naive_expression.tsv""")
+    j.command(f"""gsutil mv expression_220218/B_intermediate_expression.tsv gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/expression_files/""")
     b.run(wait=False)
 
 if __name__ == '__main__':
