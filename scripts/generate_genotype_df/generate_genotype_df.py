0e36228d20a604a110a304c30ec911d3d2331683
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-02-25 16:01:34 +1100

    Export output as PLINK format and warp script in function to launch through the analysis runner

--- scripts/generate_genotype_df/generate_genotype_df.py
+++ scripts/generate_genotype_df/generate_genotype_df.py
@@ -3,23 +3,29 @@
 """Generate genotype dfs for the association analysis"""
 
 import hail as hl
+from analysis_runner import bucket_path, output_path
 
-TOB_WGS = 'gs://cpg-tob-wgs-test/mt/v7.mt/'
+TOB_WGS = bucket_path('mt/v7.mt/')
 
-hl.init(default_reference='GRCh38')
+def query():
+    """Query script entry point."""
 
-mt = hl.read_matrix_table(TOB_WGS)
-# filter out variants that didn't pass the VQSR filter
-mt = mt.filter_rows(hl.is_missing(mt.filters))
-# VQSR does not filter out low quality genotypes. Filter these out
-mt = mt.filter_entries(mt.GQ <= 20, keep = False)
-# filter out samples with a genotype call rate > 0.8 
-n_samples = mt.count_cols()
-call_rate = 0.8
-mt = mt.filter_rows(hl.agg.sum(hl.is_missing(mt.GT)) > (n_samples * call_rate), keep=False)
-# filter out variants with MAF < 0.01
-mt = mt.filter_rows(mt.freq.AF[1] > 0.01)
-# turn files into output that we need
-# turn into plink, split up by chromosome
+    hl.init(default_reference='GRCh38')
 
+    mt = hl.read_matrix_table(TOB_WGS)
+    # filter out variants that didn't pass the VQSR filter
+    mt = mt.filter_rows(hl.is_missing(mt.filters))
+    # VQSR does not filter out low quality genotypes. Filter these out
+    mt = mt.filter_entries(mt.GQ <= 20, keep = False)
+    # filter out samples with a genotype call rate > 0.8 
+    n_samples = mt.count_cols()
+    call_rate = 0.8
+    mt = mt.filter_rows(hl.agg.sum(hl.is_missing(mt.GT)) > (n_samples * call_rate), keep=False)
+    # filter out variants with MAF < 0.01
+    mt = mt.filter_rows(mt.freq.AF[1] > 0.01)
+    # export as PLINK file
+    tob_wgs_path = output_path('tob_wgs_plink_maf01')
+    hl.export_plink(mt, tob_wgs_path, ind_id=mt.s)
 
+if __name__ == '__main__':
+    query()
