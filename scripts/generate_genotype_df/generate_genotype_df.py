c1d5f7f2f89e797f3099eda87d9659b234476f90
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-03-08 13:41:58 +1100

    Generate genotype df (#81)
    * First commit
    
    * Export output as PLINK format and warp script in function to launch through the analysis runner
    
    * Add in README
    
    * Describe reason as to why I filter at 0.8 call rate
    
    * Add in suggestion from Matt Welland and decrease n workers
    
    As per Matt Wellands suggestion, I updated filtering the `mt.filters` to handle cases where filters can be missing or a 0 length set. I also decreased the number of secondary workers to the deafult (the 20 workers was a holdover from teh last script) and I exported data to parquet, which will make reading in the genotype file much quicker.
    
    * Update scripts/generate_genotype_df/generate_genotype_df.py
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
    
    * Update scripts/generate_genotype_df/generate_genotype_df.py
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
    
    * Update scripts/generate_genotype_df/generate_genotype_df.py
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
    
    * Update scripts/generate_genotype_df/main.py
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
    
    * Update scripts/generate_genotype_df/main.py
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
    
    * Update README
    
    * Export file to pandas before saving as parquet file, since this results in a smaller file than to_spark
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>

---
+++ scripts/generate_genotype_df/generate_genotype_df.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""Generate genotype dfs for the association analysis"""
+
+import hail as hl
+from analysis_runner import bucket_path, output_path
+
+TOB_WGS = bucket_path('mt/v7.mt/')
+
+def query():
+    """Query script entry point."""
+
+    hl.init(default_reference='GRCh38')
+
+    mt = hl.read_matrix_table(TOB_WGS)
+    # filter out variants that didn't pass the VQSR filter
+    mt = mt.filter_rows(hl.len(hl.or_else(mt.filters, hl.empty_set(hl.tstr))) == 0)
+
+    # VQSR does not filter out low quality genotypes. Filter these out
+    mt = mt.filter_entries(mt.GQ <= 20, keep=False)
+    # filter out samples with a genotype call rate > 0.8 (as in the gnomAD supplementary paper)
+    n_samples = mt.count_cols()
+    call_rate = 0.8
+    mt = mt.filter_rows(hl.agg.sum(hl.is_missing(mt.GT)) > (n_samples * call_rate), keep=False)
+    # filter out variants with MAF < 0.01
+    mt = mt.filter_rows(mt.freq.AF[1] > 0.01)
+    tob_wgs_path = output_path('tob_wgs_maf01.parquet')
+    mt.rows().to_pandas()[['locus.contig','locus.position','alleles']].to_parquet(tob_wgs_path)
+
+if __name__ == '__main__':
+    query()
---
+++ scripts/generate_genotype_df/main.py
@@ -0,0 +1,21 @@
+"""Entry point for the analysis runner."""
+
+import os
+import hailtop.batch as hb
+from analysis_runner import dataproc
+
+service_backend = hb.ServiceBackend(
+    billing_project=os.getenv('HAIL_BILLING_PROJECT'), bucket=os.getenv('HAIL_BUCKET')
+)
+
+batch = hb.Batch(name='export_genotype_data', backend=service_backend)
+
+dataproc.hail_dataproc_job(
+    batch,
+    'generate_genotype_df.py',
+    max_age='4h',
+    init=['gs://cpg-reference/hail_dataproc/install_common.sh'],
+    job_name='export_genotype_data',
+)
+
+batch.run()
