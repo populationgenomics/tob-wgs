44ef0affb9c1cfb4ebedcb07521f22c15d9d0c84
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-03-15 17:18:15 +1100

    Filter to columns used in downstream analysis, convert to pandas dataframe, and split chromosomes (#83)
    * Subset all chromosomes to individual parquet files
    
    * Add comment on what I'm doing when splitting files
    
    * Filter columns before converting to pandas df, to save memory
    
    * Densify mt after reading it in
    
    * Remove all instances of 'locus.contig' in script to see if it finishes
    
    * Mkae flatten = True explicit, to see if this solves for compresssion of pandas df
    
    * Split columns into two
    
    * Save intermediate file to play around with
    
    * Add more print statements to debug failed script
    
    * Add in another print statement to debug
    
    * More debugging with print messages
    
    * COnvert locus_alleles df to string
    
    * Remove print messages and see if converting to string solves str split issue
    
    * Update path to saved files and use output_path function
    
    * Annotate rows by locus and contig, then convert to df (rather than splitting after)
    
    * Update conversion of selecting chr, contigs, and alleles
    
    This version, suggetsed by Leo, is easier to follow, and the alleles also come last (in teh correct order).
    
    * Update comment to reflect new changes
    
    * Fix pylint errors (extra carriage return and removing spaces around '='
    
    * Add 2 blank lines after function (as per pylint)
    
    * Remove carriage return at end of file (pylint)
    
    * Add new line at end of file
    
    * Remove indent from last line

--- scripts/generate_genotype_df/README.md
+++ scripts/generate_genotype_df/README.md
@@ -1,9 +1,9 @@
-# Export TOB-WGS dataset to PLINK format
+# Filter TOB-WGS dataset and export to parquet format
 
 This runs a Hail query script in Dataproc using Hail Batch in order to export the TOB-WGS genotypes, which are requirements for the association analysis. Only variants that pass VQSR and GQ filters are retained, as well as variants with an MAF > 0.01. To run, use pip to install the analysis-runner, then execute the following command:
 
 ```sh
 analysis-runner --dataset tob-wgs \
---access-level standard --output-dir "tob_wgs_plink_maf01/v0" \
---description "TOB plink" python3 main.py
+--access-level standard --output-dir "scrna-seq/grch38_association_files/genotype_files/" \
+--description "TOB parquet" python3 main.py
 ```
--- scripts/generate_genotype_df/generate_genotype_df.py
+++ scripts/generate_genotype_df/generate_genotype_df.py
@@ -14,9 +14,9 @@ def query():
     hl.init(default_reference='GRCh38')
 
     mt = hl.read_matrix_table(TOB_WGS)
+    mt = hl.experimental.densify(mt)
     # filter out variants that didn't pass the VQSR filter
     mt = mt.filter_rows(hl.len(hl.or_else(mt.filters, hl.empty_set(hl.tstr))) == 0)
-
     # VQSR does not filter out low quality genotypes. Filter these out
     mt = mt.filter_entries(mt.GQ <= 20, keep=False)
     # filter out samples with a genotype call rate > 0.8 (as in the gnomAD supplementary paper)
@@ -25,8 +25,16 @@ def query():
     mt = mt.filter_rows(hl.agg.sum(hl.is_missing(mt.GT)) > (n_samples * call_rate), keep=False)
     # filter out variants with MAF < 0.01
     mt = mt.filter_rows(mt.freq.AF[1] > 0.01)
-    tob_wgs_path = output_path('tob_wgs_maf01.parquet')
-    mt.rows().to_pandas()[['locus.contig', 'locus.position', 'alleles']].to_parquet(tob_wgs_path)
+    # select alleles and locus (contig and position must be selected separately),
+    # then convert to pandas
+    t = mt.rows()
+    t = t.key_by(contig=t.locus.contig, position=t.locus.position)
+    t = t.select(t.alleles)
+    pd = t.to_pandas(flatten=True)
+    # expand locus to two columns and rename 
+    # save each chromosome to an individual file
+    for chr in set(pd['contig']): 
+        pd.loc[pd['contig'] == chr].to_parquet(output_path(f'tob_genotype_maf01_{chr}.parquet'))
 
 
 if __name__ == '__main__':
