40c0d1639df3a2a539782d318f908368de2a85fa
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-03-24 10:18:56 +1100

    Copy expression files Copy ASDC expression data to debug. (This cell type failed in the PEER script; see log here: https://batch.hail.populationgenomics.org.au/batches/29672/jobs/2)

---
+++ scripts/data_transfer/copy_expression_data.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -ex
+
+gsutil cp gs://cpg-tob-wgs-main/scrna-seq/grch38_association_files/expression_files/ASDC_expression.tsv gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/expression_files/
