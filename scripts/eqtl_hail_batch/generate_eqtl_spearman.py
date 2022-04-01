31644986999018baa218733687bb3a32dcd3b82b
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-04-01 11:04:52 +1100

    Temporarily remove to_csv command
    This may be causing the error: `ImportError: Missing optional dependency 'fsspec'`. See https://batch.hail.populationgenomics.org.au/batches/30502/jobs/1

--- scripts/eqtl_hail_batch/generate_eqtl_spearman.py
+++ scripts/eqtl_hail_batch/generate_eqtl_spearman.py
@@ -81,7 +81,7 @@ def calculate_log_cpm(expression_df, output_prefix):
     log_cpm = np.log(cpm_df + 1)
     # add sampleids back in
     log_cpm = log_cpm.assign(sampleid=list(sample_ids))
-    log_cpm.to_csv(os.path.join(output_prefix, f'log_cpm.tsv'), index=False)
+    # log_cpm.to_csv(os.path.join(output_prefix, f'log_cpm.tsv'), index=False)
 
 
 def calculate_residuals(expression_df, covariate_df, output_prefix):
@@ -110,7 +110,7 @@ def calculate_residuals(expression_df, covariate_df, output_prefix):
     residual_df = pd.DataFrame(list(map(calculate_gene_residual, gene_ids))).T
     residual_df.columns = gene_ids
     residual_df = residual_df.assign(sampleid=list(sample_ids))
-    residual_df.to_csv(os.path.join(output_prefix, f'log_residuals.tsv'), index=False)
+    # residual_df.to_csv(os.path.join(output_prefix, f'log_residuals.tsv'), index=False)
 
     return residual_df
 
