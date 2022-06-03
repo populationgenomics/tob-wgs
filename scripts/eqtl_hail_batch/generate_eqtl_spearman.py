8ccebc4c68579b0f22a83307e090fb436dce3abb
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-06-03 17:39:29 +1000

    Fix error when adding in new columns
    getting the error: 'Pandas doesn't allow columns to be created via a new attribute name'. Solved as in this Stack Overflow post: https://stackoverflow.com/questions/53178162/userwarning-pandas-doesnt-allow-columns-to-be-created-via-a-new-attribute-name

--- scripts/eqtl_hail_batch/generate_eqtl_spearman.py
+++ scripts/eqtl_hail_batch/generate_eqtl_spearman.py
@@ -418,12 +418,13 @@ def run_spearman_correlation_scatter(
     # chromosome must be turned into a number solely
     # note, 'chr' + chromosome number was necessary in the previous step, 
     # as 'chr' indicates GrCh38 format
-    spearman_df.chrom = spearman_df.chrom.str.split('chr', expand=True)[1]
+    spearman_df['chrom'] = spearman_df.chrom.str.split('chr', expand=True)[1]
     print(f'Spearman df chrom is {spearman_df.chrom}')
     # add association ID annotation after adding in alleles, a1, and a2
-    spearman_df.association_id = spearman_df.apply(lambda x: ':'.join(x[['chrom', 'bp', 'a1', 'a2', 'gene_symbol', 'cell_type_id', 'round']]), axis=1)
-    spearman_df.variant_id = spearman_df.apply(lambda x: ':'.join(x[['chrom', 'bp', 'a2']]), axis=1)
-    spearman_df.snp_id = spearman_df.apply(lambda x: ':'.join(x[['chrom', 'bp', 'a1', 'a2']]), axis=1)
+    spearman_df['association_id'] = spearman_df.apply(lambda x: ':'.join(x[['chrom', 'bp', 'a1', 'a2', 'gene_symbol', 'cell_type_id', 'round']]), axis=1)
+    spearman_df['variant_id'] = spearman_df.apply(lambda x: ':'.join(x[['chrom', 'bp', 'a2']]), axis=1)
+    spearman_df['snp_id'] = spearman_df.apply(lambda x: ':'.join(x[['chrom', 'bp', 'a1', 'a2']]), axis=1)
+    
     return spearman_df
 
 
