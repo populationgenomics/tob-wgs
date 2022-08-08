a24a76381e58c2550f8ae09f7e0a83847ca48e97
Anna Cuomo <annacuomo92@gmail.com>
2022-08-08 14:38:41 +1000

    add -m for multi-threaded operations as per Leo's suggestion
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>

--- scripts/data_transfer/copy_uploaded_CellRegMap_input_files.sh
+++ scripts/data_transfer/copy_uploaded_CellRegMap_input_files.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-gsutil mv -r "gs://cpg-tob-wgs-main-upload/CellRegMap_input_files/" gs://cpg-tob-wgs-main/scrna-seq/
+gsutil -m mv gs://cpg-tob-wgs-main-upload/CellRegMap_input_files/ gs://cpg-tob-wgs-main/scrna-seq/
 
