6189c5afdd520246c34fa55695afc76fba2a9798
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-08-12 16:43:30 +1000

    format with black

--- scripts/rv_expression_association/get_rv_outliers.py
+++ scripts/rv_expression_association/get_rv_outliers.py
@@ -20,21 +20,24 @@ import pandas as pd
 # create the following table:
 # donor ID | gene ID | variant ID | position | CADD | MAF (OneK1K) | MAF (gnomad) | ..
 
+
 def main(
     onek1k_id: str,
     gene_name: str,
     chrom: str,
 ):
-    
+
     # 'CPG9951' (943_944) is an expression outlier for gene: 'IGLL5'
 
-    # file matching OneK1K IDs to CPG (internal) and TOB (external) IDs 
-    sample_key_filename = 'gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/OneK1K_CPG_IDs.tsv'
+    # file matching OneK1K IDs to CPG (internal) and TOB (external) IDs
+    sample_key_filename = (
+        'gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/OneK1K_CPG_IDs.tsv'
+    )
     sample_key_df = pd.read_csv(sample_key_filename, sep='\t', index_col=0)
 
     cpg_id = sample_key_df[sample_key_df['OneK1K_ID'] == onek1k_id]['InternalID']
     # switch print to log
-    print(cpg_id) # 'CPG9951' 
+    print(cpg_id)  # 'CPG9951'
 
     # define output filename and check if it already exists
     output_filename = cpg_id + '-' + gene_name + '.csv'
@@ -42,20 +45,28 @@ def main(
     # get VEP-annotated WGS object (hail matrix table)
     mt = hl.read_matrix_table('gs://cpg-tob-wgs-test/tob_wgs_vep/v1/vep105_GRCh38.mt')
 
-    # select matrix down to that one donor 
+    # select matrix down to that one donor
     donor_mt = mt.filter_cols(mt.s == cpg_id)
 
     # get gene body position (start and end) and build interval
     # include variants up to 10kb up- and downstream
-    gene_file = 'gs://cpg-tob-wgs-main/scrna-seq/grch38_association_files/gene_location_files/GRCh38_geneloc_chr'+chrom+'.tsv'
+    gene_file = (
+        'gs://cpg-tob-wgs-main/scrna-seq/grch38_association_files/gene_location_files/GRCh38_geneloc_chr'
+        + chrom
+        + '.tsv'
+    )
     gene_df = pd.read_csv(gene_file, sep='\t', index_col=0)
-    interval_start = float(gene_df[gene_df['gene_name'] == gene_name]['start'])-10000
-    interval_end = float(gene_df[gene_df['gene_name'] == gene_name]['end'])+10000
+    interval_start = float(gene_df[gene_df['gene_name'] == gene_name]['start']) - 10000
+    interval_end = float(gene_df[gene_df['gene_name'] == gene_name]['end']) + 10000
 
     # get gene-specific genomic interval
-    gene_interval = 'chr'+chrom+':'+str(interval_start)+'-'+str(interval_end) # 'chr22:23219960-23348287'
-    print(gene_interval) # switch print to log
-    donor_mt = hl.filter_intervals(donor_mt, [hl.parse_locus_interval(gene_interval, reference_genome='GRCh38')])
+    gene_interval = (
+        'chr' + chrom + ':' + str(interval_start) + '-' + str(interval_end)
+    )  # 'chr22:23219960-23348287'
+    print(gene_interval)  # switch print to log
+    donor_mt = hl.filter_intervals(
+        donor_mt, [hl.parse_locus_interval(gene_interval, reference_genome='GRCh38')]
+    )
 
     # remove variants for which this individual is 0/0
     donor_mt = hl.variant_qc(donor_mt)
@@ -67,22 +78,24 @@ def main(
     donor_mt = donor_mt.filter_rows(hl.len(donor_mt.alleles) == 2)
 
     # annotate variants with CADD scores, gnomad etc
-    ref_ht = hl.read_table('gs://cpg-reference/seqr/v0-1/combined_reference_data_grch38-2.0.4.ht')
+    ref_ht = hl.read_table(
+        'gs://cpg-reference/seqr/v0-1/combined_reference_data_grch38-2.0.4.ht'
+    )
     donor_mt = donor_mt.annotate_rows(
         cadd=ref_ht[donor_mt.row_key].cadd,
-        gnomad_genomes=ref_ht[donor_mt.row_key].gnomad_genomes
+        gnomad_genomes=ref_ht[donor_mt.row_key].gnomad_genomes,
     )
 
     # get CADD scores
     cadd_list = donor_mt.cadd.PHRED.collect()
 
     results_data = {
-        'onek1k_id': onek1k_id, 
+        'onek1k_id': onek1k_id,
         'cpg_id': cpg_id,
         'gene_name': gene_name,
         'variant_id': donor_mt.row_key[0].collect(),
-        'cadd': cadd_list
-        }
+        'cadd': cadd_list,
+    }
     df = pd.DataFrame(results_data)
     df.to_csv(output_filename)
 
