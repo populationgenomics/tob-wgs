7c9ec0423e7301179f77b675111f79a810fd99ad
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-08-16 15:49:48 +1000

    add logging, output folder

--- scripts/rv_expression_association/get_rv_outliers.py
+++ scripts/rv_expression_association/get_rv_outliers.py
@@ -1,21 +1,33 @@
+import os
+import sys
 import click
+import logging
 import hail as hl
 import pandas as pd
 
-# get OneK1K sample ID as an argument using click
+# use logging to print statements, display at info level
+logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
+
+# get OneK1K sample ID and gene name as arguments using click
 @click.command()
 @click.option('--onek1k-id', required=True)
 @click.option('--gene-name', required=True)
 @click.option('--chrom', required=True)
+@click.option('--output_folder', required=True)
 
 # for a given individual,
-# and gene(s?) for which that individual is an expression outlier
-# get all variants within a window, and annotate with the following:
+# and gene for which that individual is an expression outlier
+# get all variants within a window, 
+
+# then filter for variants that:
+# are alt (0/1 or 1/1) for that individual
+# are biallelic SNVs
+# have regulatory consequences based on VEP annotations
+
+# also annotate variants with the following:
 # CADD score
-# MAF within OneK1K
+# MAF within the OneK1K sample
 # MAF in gnomad
-# regulatory information from VEP
-# other?
 
 # create the following table:
 # donor ID | gene ID | variant ID | position | CADD | MAF (OneK1K) | MAF (gnomad) | ..
@@ -25,6 +37,7 @@ def main(
     onek1k_id: str,
     gene_name: str,
     chrom: str,
+    output_folder: str,
 ):
 
     # 'CPG9951' (943_944) is an expression outlier for gene: 'IGLL5'
@@ -36,17 +49,24 @@ def main(
     sample_key_df = pd.read_csv(sample_key_filename, sep='\t', index_col=0)
 
     cpg_id = sample_key_df[sample_key_df['OneK1K_ID'] == onek1k_id]['InternalID']
-    # switch print to log
-    print(cpg_id)  # 'CPG9951'
+    logging.info("CPG ID: {}".format(cpg_id)) # e.g., 'CPG9951'
 
     # define output filename and check if it already exists
-    output_filename = cpg_id + '-' + gene_name + '.csv'
+    output_filename = os.path.join(output_folder, cpg_id, '-', gene_name, '.csv')
+    logging.info("Output file: {}".format(output_filename)) 
+
+    # skip if file already exists
+    if os.path.exists(output_filename):
+        logging.info("File already exists, exiting")
+        sys.exit()
 
     # get VEP-annotated WGS object (hail matrix table)
     mt = hl.read_matrix_table('gs://cpg-tob-wgs-test/tob_wgs_vep/v1/vep105_GRCh38.mt')
 
     # select matrix down to that one donor
     donor_mt = mt.filter_cols(mt.s == cpg_id)
+
+    logging.info("Number of total variants: {}".format(donor_mt.count()[0]))
 
     # check this file in the future (GENCODE??)
     # get gene body position (start and end) and build interval
@@ -68,15 +88,22 @@ def main(
     donor_mt = hl.filter_intervals(
         donor_mt, [hl.parse_locus_interval(gene_interval, reference_genome='GRCh38')]
     )
+    logging.info("Number of variants within interval: {}".format(donor_mt.count()[0]))
 
     # remove variants for which this individual is 0/0
     donor_mt = hl.variant_qc(donor_mt)
     donor_mt = donor_mt.filter_rows(donor_mt.variant_qc.n_non_ref > 0)
+    logging.info("Number of non-ref variants for this indvidual: {}".format(donor_mt.count()[0]))
 
     # focus on SNVs for now
     donor_mt = donor_mt.filter_rows(donor_mt.vep.variant_class == 'SNV')
     # filter for biallelic only
     donor_mt = donor_mt.filter_rows(hl.len(donor_mt.alleles) == 2)
+    logging.info("Number of variants after filtering for biallelic SNVs: {}".format(donor_mt.count()[0]))
+
+    # filter to only variants with some regulatory consequences
+    donor_mt = donor_mt.filter_rows(hl.len(donor_mt.vep.regulatory_feature_consequences) > 0)
+    logging.info("Number of variants after filtering for variants with regulatory consequences: {}".format(donor_mt.count()[0]))
 
     # annotate variants with CADD scores, gnomad etc
     ref_ht = hl.read_table(
@@ -90,7 +117,8 @@ def main(
     # get CADD scores
     cadd_list = donor_mt.cadd.PHRED.collect()
 
-    # get gnomad MAF (both popmax and nfe)
+    # get gnomad MAF (popmax now, need NFE too?)
+    maf_popmax = donor_mt.gnomad_genomes.AF_POPMAX_OR_GLOBAL.collect()
 
     # get MAF within sample
     # by selecting the appropriate rows in the full object
@@ -98,9 +126,10 @@ def main(
     mt = mt.filter_rows(hl.set(relevant_loci).contains(mt.row_key))
     # calculating their variant QC
     mt = hl.variant_qc(mt)
-    maf_sample_list = mt.AF[0].collect()
+    maf_sample_list = mt.variant_qc.AF[0].collect()
 
-    # get relevant regulatory info from VEP (enough?)
+    # get relevant regulatory info from VEP (biotype)
+    regulatory_consequences = donor_mt.vep.regulatory_feature_consequences['biotype'].collect()
 
     results_data = {
         'onek1k_id': onek1k_id,
@@ -108,7 +137,9 @@ def main(
         'gene_name': gene_name,
         'variant_id': donor_mt.row_key[0].collect(),
         'cadd': cadd_list,
-        'maf (OneK1K)': maf_sample_list,
+        'maf_onek1k': maf_sample_list,
+        'maf_gnomad_popmax': maf_popmax,
+        'regulatory_consequences': regulatory_consequences,
     }
     df = pd.DataFrame(results_data)
     df.to_csv(output_filename)
@@ -116,3 +147,5 @@ def main(
 
 if __name__ == '__main__':
     main()
+
+
