3b282705cc83f16a6dadf421b0fa9edef933b9c5
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-08-23 15:49:21 +1000

    Test plotting two figures

--- scripts/plot/plot_data.py
+++ scripts/plot/plot_data.py
@@ -7,8 +7,6 @@ Test plotting
 
 import hail as hl
 from bokeh.io.export import get_screenshot_as_png
-from bokeh.embed import file_html
-from bokeh.resources import CDN
 from cpg_utils.hail_batch import (
     dataset_path,
     output_path,
@@ -33,16 +31,13 @@ def main():
     )
     mt = mt.filter_rows(hl.len(hl.or_else(mt.filters, hl.empty_set(hl.tstr))) == 0)
     p1 = hl.plot.histogram(mt.variant_qc.AF[1])
-    p1_filename = output_path('histogram_maf_pre_filter.png', 'web')
+    p1_filename = output_path('plot1.png', 'web')
     with hl.hadoop_open(p1_filename, 'wb') as f:
         get_screenshot_as_png(p1).save(f, format='PNG')
-    html = file_html(p1, CDN, 'my plot')
-    plot_filename_html = output_path(f'plot1.html', 'web')
-    with hl.hadoop_open(plot_filename_html, 'w') as f:
-        f.write(html)
+    p2 = hl.plot.histogram(mt.variant_qc.AF[1])
     p2_filename = output_path('histogram_plot2.png', 'web')
     with hl.hadoop_open(p2_filename, 'wb') as f:
-        get_screenshot_as_png(p1).save(f, format='PNG')
+        get_screenshot_as_png(p2).save(f, format='PNG')
 
 
 if __name__ == '__main__':
