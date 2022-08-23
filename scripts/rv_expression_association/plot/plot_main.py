8e9925ac5d605a39ce41fc962d0f5176e4890637
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-08-24 09:03:59 +1000

    Plot in Hail Batch

--- scripts/rv_expression_association/plot/plot_main.py
+++ scripts/rv_expression_association/plot/plot_main.py
@@ -2,32 +2,33 @@
 
 """Entry point for the analysis runner."""
 
-import hailtop.batch as hb
-from analysis_runner import dataproc
+from bokeh.io.export import get_screenshot_as_png
 from cpg_utils.config import get_config
-from cpg_utils.hail_batch import (
-    remote_tmpdir,
-    copy_common_env,
-)
+from cpg_utils.hail_batch import (dataset_path, init_batch, output_path)
+import hailtop.batch as hb
 
-backend = hb.ServiceBackend(
-    billing_project=get_config()['hail']['billing_project'],
-    remote_tmpdir=remote_tmpdir(),
-)
+VEP_MT = dataset_path('tob_wgs_vep/v1/vep105_GRCh38.mt')
 
-batch = hb.Batch(
-    name='plot_figure',
-    backend=backend,
-    default_python_image=get_config()['workflow']['driver_image'],
-)
 
-job = dataproc.hail_dataproc_job(
-    batch,
-    f'plot_data.py',
-    max_age='1h',
-    init=['gs://cpg-reference/hail_dataproc/install_common.sh'],
-    job_name=f'plot_data',
-)
-copy_common_env(job)
+def main():
+    init_batch()
 
-batch.run(wait=False)
+    mt = hl.read_matrix_table(VEP_MT)
+    # mt = hl.experimental.densify(mt)
+    mt = hl.variant_qc(mt)
+    mt = hl.filter_intervals(
+        mt,
+        [hl.parse_locus_interval('chr22:23704425-23802743', reference_genome='GRCh38')],
+    )
+    mt = mt.filter_rows(hl.len(hl.or_else(mt.filters, hl.empty_set(hl.tstr))) == 0)
+    p1 = hl.plot.histogram(mt.variant_qc.AF[1])
+    p1_filename = output_path('histogram_maf_pre_filter.png', 'web')
+    with hl.hadoop_open(p1_filename, 'wb') as f:
+        get_screenshot_as_png(p1).save(f, format='PNG')
+
+    p2_filename = output_path('histogram_plot2.png', 'web')
+    with hl.hadoop_open(p2_filename, 'wb') as f:
+        get_screenshot_as_png(p1).save(f, format='PNG')
+
+if __name__ == '__main__':
+    main()
