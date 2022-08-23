6ef15c0ea1625720c536b17f6ce1a26adc282433
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-08-23 13:52:20 +1000

    add second plot to debug

--- scripts/rv_expression_association/plot/plot_data.py
+++ scripts/rv_expression_association/plot/plot_data.py
@@ -35,6 +35,10 @@ def main():
     with hl.hadoop_open(p1_filename, 'wb') as f:
         get_screenshot_as_png(p1).save(f, format='PNG')
 
+    p2_filename = output_path('histogram_plot2.png', 'web')
+    with hl.hadoop_open(p2_filename, 'wb') as f:
+        get_screenshot_as_png(p1).save(f, format='PNG')
+
     # sample = 'CPG18'
     # donor_mt = mt.filter_cols(mt.s == sample)
     # donor_mt = donor_mt.filter_rows(hl.agg.any(donor_mt.GT.is_non_ref()))
