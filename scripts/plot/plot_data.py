7f4e14be7625816662ee0fca4be5f4156e1ba1e1
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-08-23 13:40:59 +1000

    Test plotting html file

--- scripts/plot/plot_data.py
+++ scripts/plot/plot_data.py
@@ -7,10 +7,13 @@ Test plotting
 
 import hail as hl
 from bokeh.io.export import get_screenshot_as_png
+from bokeh.embed import file_html
+from bokeh.resources import CDN
 from cpg_utils.hail_batch import (
     dataset_path,
     output_path,
 )
+
 
 VEP_MT = dataset_path('tob_wgs_vep/v1/vep105_GRCh38.mt')
 
@@ -23,7 +26,6 @@ def main():
     hl.init(default_reference='GRCh38')
 
     mt = hl.read_matrix_table(VEP_MT)
-    mt = hl.experimental.densify(mt)
     mt = hl.variant_qc(mt)
     mt = hl.filter_intervals(
         mt,
@@ -34,15 +36,13 @@ def main():
     p1_filename = output_path('histogram_maf_pre_filter.png', 'web')
     with hl.hadoop_open(p1_filename, 'wb') as f:
         get_screenshot_as_png(p1).save(f, format='PNG')
-
-    sample = 'CPG18'
-    donor_mt = mt.filter_cols(mt.s == sample)
-    donor_mt = donor_mt.filter_rows(hl.agg.any(donor_mt.GT.is_non_ref()))
-    mt = mt.semi_join_rows(donor_mt.rows())
-    p2 = hl.plot.histogram(mt.variant_qc.AF[1])
-    p2_filename = output_path('histogram_maf_post_filter.png', 'web')
+    html = file_html(p1, CDN, 'my plot')
+    plot_filename_html = output_path(f'plot1.html', 'web')
+    with hl.hadoop_open(plot_filename_html, 'w') as f:
+        f.write(html)
+    p2_filename = output_path('histogram_plot2.png', 'web')
     with hl.hadoop_open(p2_filename, 'wb') as f:
-        get_screenshot_as_png(p2).save(f, format='PNG')
+        get_screenshot_as_png(p1).save(f, format='PNG')
 
 
 if __name__ == '__main__':
