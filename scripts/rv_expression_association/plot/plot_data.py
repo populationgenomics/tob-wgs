dda27847b00b1936cdcdb58a9a3d25dd2332f1aa
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-08-23 10:52:41 +1000

    comment out expensive lines to test

--- scripts/rv_expression_association/plot/plot_data.py
+++ scripts/rv_expression_association/plot/plot_data.py
@@ -23,7 +23,7 @@ def main():
     hl.init(default_reference='GRCh38')
 
     mt = hl.read_matrix_table(VEP_MT)
-    mt = hl.experimental.densify(mt)
+    # mt = hl.experimental.densify(mt)
     mt = hl.variant_qc(mt)
     mt = hl.filter_intervals(
         mt,
@@ -35,14 +35,14 @@ def main():
     with hl.hadoop_open(p1_filename, 'wb') as f:
         get_screenshot_as_png(p1).save(f, format='PNG')
 
-    sample = 'CPG18'
-    donor_mt = mt.filter_cols(mt.s == sample)
-    donor_mt = donor_mt.filter_rows(hl.agg.any(donor_mt.GT.is_non_ref()))
-    mt = mt.semi_join_rows(donor_mt.rows())
-    p2 = hl.plot.histogram(mt.variant_qc.AF[1])
-    p2_filename = output_path('histogram_maf_post_filter.png', 'web')
-    with hl.hadoop_open(p2_filename, 'wb') as f:
-        get_screenshot_as_png(p2).save(f, format='PNG')
+    # sample = 'CPG18'
+    # donor_mt = mt.filter_cols(mt.s == sample)
+    # donor_mt = donor_mt.filter_rows(hl.agg.any(donor_mt.GT.is_non_ref()))
+    # mt = mt.semi_join_rows(donor_mt.rows())
+    # p2 = hl.plot.histogram(mt.variant_qc.AF[1])
+    # p2_filename = output_path('histogram_maf_post_filter.png', 'web')
+    # with hl.hadoop_open(p2_filename, 'wb') as f:
+    #     get_screenshot_as_png(p2).save(f, format='PNG')
 
 
 if __name__ == '__main__':
