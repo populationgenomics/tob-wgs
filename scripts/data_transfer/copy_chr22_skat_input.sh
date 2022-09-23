deddaca0bc6a157a2a4ffe6d78b7582fb8ae5a02
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-09-23 14:28:16 +1000

    add transfer script

---
+++ scripts/data_transfer/copy_chr22_skat_input.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -ex
+
+gsutil -m mv gs://cpg-tob-wgs-main/CellRegMap_input_files/plink_files/plink_chr22.bed gs://cpg-tob-wgs-test/v0/skat/
+
+gsutil -m mv gs://cpg-tob-wgs-main/CellRegMap_input_files/plink_files/plink_chr22.bim gs://cpg-tob-wgs-test/v0/skat/
+
+gsutil -m mv gs://cpg-tob-wgs-main/CellRegMap_input_files/plink_files/plink_chr22.fam gs://cpg-tob-wgs-test/v0/skat/
+
+gsutil -m mv gs://cpg-tob-wgs-main/CellRegMap_input_files/GRM/grm_wide.csv gs://cpg-tob-wgs-test/v0/skat/
+
+gsutil -m mv gs://cpg-tob-wgs-main/CellRegMap_input_files/expression_objects/sce22.h5ad gs://cpg-tob-wgs-test/v0/skat/
