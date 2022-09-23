8c52d5b85c61e0efa6a4e345aa33e22b20529c86
Anna Cuomo <annacuomo92@gmail.com>
2022-09-23 16:02:56 +1000

    Update copy_chr22_skat_input.sh

--- scripts/data_transfer/copy_chr22_skat_input.sh
+++ scripts/data_transfer/copy_chr22_skat_input.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-gsutil -m mv gs://cpg-tob-wgs-main/CellRegMap_input_files/plink_files/plink_chr22.bed gs://cpg-tob-wgs-test/v0/skat/
-gsutil -m mv gs://cpg-tob-wgs-main/CellRegMap_input_files/plink_files/plink_chr22.bim gs://cpg-tob-wgs-test/v0/skat/
-gsutil -m mv gs://cpg-tob-wgs-main/CellRegMap_input_files/plink_files/plink_chr22.fam gs://cpg-tob-wgs-test/v0/skat/
-gsutil -m mv gs://cpg-tob-wgs-main/CellRegMap_input_files/GRM/grm_wide.csv gs://cpg-tob-wgs-test/v0/skat/
-gsutil -m mv gs://cpg-tob-wgs-main/CellRegMap_input_files/expression_objects/sce22.h5ad gs://cpg-tob-wgs-test/v0/skat/
+gsutil -m cp gs://cpg-tob-wgs-main/CellRegMap_input_files/plink_files/plink_chr22.bed \
+    gs://cpg-tob-wgs-main/CellRegMap_input_files/plink_files/plink_chr22.bim \
+    gs://cpg-tob-wgs-main/CellRegMap_input_files/plink_files/plink_chr22.fam \
+    gs://cpg-tob-wgs-main/CellRegMap_input_files/GRM/grm_wide.csv \
+    gs://cpg-tob-wgs-main/CellRegMap_input_files/expression_objects/sce22.h5ad gs://cpg-tob-wgs-test/v0/skat/
