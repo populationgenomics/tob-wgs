b81fe6665f297a8e5b6badbe1e2c703afda221fd
Anna Cuomo <annacuomo92@gmail.com>
2022-09-23 14:28:57 +1000

    Update copy_chr22_skat_input.sh

--- scripts/data_transfer/copy_chr22_skat_input.sh
+++ scripts/data_transfer/copy_chr22_skat_input.sh
@@ -3,11 +3,7 @@
 set -ex
 
 gsutil -m mv gs://cpg-tob-wgs-main/CellRegMap_input_files/plink_files/plink_chr22.bed gs://cpg-tob-wgs-test/v0/skat/
-
 gsutil -m mv gs://cpg-tob-wgs-main/CellRegMap_input_files/plink_files/plink_chr22.bim gs://cpg-tob-wgs-test/v0/skat/
-
 gsutil -m mv gs://cpg-tob-wgs-main/CellRegMap_input_files/plink_files/plink_chr22.fam gs://cpg-tob-wgs-test/v0/skat/
-
 gsutil -m mv gs://cpg-tob-wgs-main/CellRegMap_input_files/GRM/grm_wide.csv gs://cpg-tob-wgs-test/v0/skat/
-
 gsutil -m mv gs://cpg-tob-wgs-main/CellRegMap_input_files/expression_objects/sce22.h5ad gs://cpg-tob-wgs-test/v0/skat/
