b12b1d0ad29139975ecc142bcec4c197221bdad3
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-09-23 13:35:09 +1000

    print out numbers of variants at each step

--- scripts/rv_expression_association/igll5_vep.py
+++ scripts/rv_expression_association/igll5_vep.py
@@ -13,6 +13,8 @@ def main():
     mt = hl.read_matrix_table(MT)
     mt = hl.experimental.densify(mt)
 
+    print(mt.count())
+
     # filter out low quality variants
     mt = mt.filter_rows(hl.len(hl.or_else(mt.filters, hl.empty_set(hl.tstr))) == 0)
 
@@ -21,16 +23,20 @@ def main():
     mt = mt.filter_rows(hl.is_snp(mt.alleles[0], mt.alleles[1]))
 
     # annotate using VEP
-    # vep_ht = reference_path('tob_wgs_vep/104/vep104.3_GRCh38.ht')
     vep_ht = reference_path('v0/IGLL5_50K_window_vep.ht')
     mt = mt.annotate_rows(vep=vep_ht[mt.row_key].vep)
+
+    print(mt.count())
 
     # filter rare variants only (MAF < 5%)
     mt = hl.variant_qc(mt)
     rv_mt = mt.filter_rows(mt.variant_qc.AF[1] < 0.05)
 
+    print(rv_mt.count())
+
     # filter variants found to have regulatory effects
     filtered_mt = rv_mt.filtered_mt(hl.len(rv_mt.vep.regulatory_feature_consequences['biotype']) > 0)
+    print(filtered_mt.count())
 
     # export MT object to PLINK
     export_plink(filtered_mt, 'plink_files/igll5_rare_regulatory', ind_id=mt.s)
