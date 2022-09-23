9de346ecaa9896e7713f5154cd9802f611646f84
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-09-23 17:03:59 +1000

    test opening file with scanpy

--- scripts/rv_expression_association/prepare_inputs.py
+++ scripts/rv_expression_association/prepare_inputs.py
@@ -11,17 +11,19 @@ from limix.qc import quantile_gaussianize
 # use logging to print statements, display at info level
 logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
 
+phenotype_file = "gs://cpg-tob-wgs-test/v0/skat/sce22.h5ad"
+
 def main(
     # chrom: str,
-    gene_name: str,
-    sample_mapping_file: str,
-    genotype_file: str,
-    phenotype_file: str,
-    # context_file: str,
-    kinship_file: str,
-    # feature_variant_file: str,
-    output_folder: str,
-    # n_contexts: int = 10,
+    # gene_name: str,
+    # sample_mapping_file: str,
+    # genotype_file: str,
+    # phenotype_file: str,
+    # # context_file: str,
+    # kinship_file: str,
+    # # feature_variant_file: str,
+    # output_folder: str,
+    # # n_contexts: int = 10,
 ):
 """
 Prepare input files to be run using SKAT
@@ -32,20 +34,20 @@ Prepare input files to be run using SKAT
     ######################################
 
     ## this file will map cells to donors
-    sample_mapping = pd.read_csv(
-        sample_mapping_file,
-        dtype={
-            "individual_long": str,
-            "genotype_individual_id": str,
-            "phenotype_sample_id": str,
-        },
-        index_col=0,
-    )
+    # sample_mapping = pd.read_csv(
+    #     sample_mapping_file,
+    #     dtype={
+    #         "individual_long": str,
+    #         "genotype_individual_id": str,
+    #         "phenotype_sample_id": str,
+    #     },
+    #     index_col=0,
+    # )
 
-    ## extract unique individuals
-    donors0 = sample_mapping["genotype_individual_id"].unique()
-    donors0.sort()
-    logging.info("Number of unique donors: {}".format(len(donors0)))
+    # ## extract unique individuals
+    # donors0 = sample_mapping["genotype_individual_id"].unique()
+    # donors0.sort()
+    # logging.info("Number of unique donors: {}".format(len(donors0)))
 
     ######################################
     ########### phenotype file ###########
@@ -66,71 +68,72 @@ Prepare input files to be run using SKAT
     mat_df = pd.DataFrame(
         data=mat.T, index=adata.raw.var.index, columns=adata.obs.index
     )
+    print(mar_df.head())
     # turn into xr array
-    phenotype = xr.DataArray(
-        mat_df.values,
-        dims=["trait", "cell"],
-        coords={"trait": mat_df.index.values, "cell": mat_df.columns.values},
-    )
-    phenotype = phenotype.sel(cell=sample_mapping["phenotype_sample_id"].values)
+    # phenotype = xr.DataArray(
+    #     mat_df.values,
+    #     dims=["trait", "cell"],
+    #     coords={"trait": mat_df.index.values, "cell": mat_df.columns.values},
+    # )
+    # phenotype = phenotype.sel(cell=sample_mapping["phenotype_sample_id"].values)
 
-    # delete large files to free up memory
-    del mat
-    del mat_df
+    # # delete large files to free up memory
+    # del mat
+    # del mat_df
 
-    # select gene
-    y = phenotype.sel(trait=gene_name)
-    y = quantile_gaussianize(y)
-    y.c = y.values.reshape(y.shape[0], 1)
+    # # select gene
+    # y = phenotype.sel(trait=gene_name)
+    # y = quantile_gaussianize(y)
+    # y.c = y.values.reshape(y.shape[0], 1)
 
-    del phenotype  # delete to free up memory
+    # del phenotype  # delete to free up memory
 
-    ######################################
-    ############ kinship file ############
-    ######################################
+    # ######################################
+    # ############ kinship file ############
+    # ######################################
 
-    ## read in GRM (genotype relationship matrix; kinship matrix)
-    K = pd.read_csv(kinship_file, index_col=0)
-    K.index = K.index.astype("str")
-    assert all(K.columns == K.index)  # symmetric matrix, donors x donors
+    # ## read in GRM (genotype relationship matrix; kinship matrix)
+    # K = pd.read_csv(kinship_file, index_col=0)
+    # K.index = K.index.astype("str")
+    # assert all(K.columns == K.index)  # symmetric matrix, donors x donors
 
-    K = xr.DataArray(
-        K.values,
-        dims=["sample_0", "sample_1"],
-        coords={"sample_0": K.columns, "sample_1": K.index},
-    )
-    K = K.sortby("sample_0").sortby("sample_1")
-    donors = sorted(set(list(K.sample_0.values)).intersection(donors0))
-    logging.info("Number of donors after kinship intersection: {}".format(len(donors)))
+    # K = xr.DataArray(
+    #     K.values,
+    #     dims=["sample_0", "sample_1"],
+    #     coords={"sample_0": K.columns, "sample_1": K.index},
+    # )
+    # K = K.sortby("sample_0").sortby("sample_1")
+    # donors = sorted(set(list(K.sample_0.values)).intersection(donors0))
+    # logging.info("Number of donors after kinship intersection: {}".format(len(donors)))
 
-    ## subset to relevant donors
-    K = K.sel(sample_0=donors, sample_1=donors)
-    assert all(K.sample_0 == donors)
-    assert all(K.sample_1 == donors)
+    # ## subset to relevant donors
+    # K = K.sel(sample_0=donors, sample_1=donors)
+    # assert all(K.sample_0 == donors)
+    # assert all(K.sample_1 == donors)
 
-    del K  # delete K to free up memory
+    # del K  # delete K to free up memory
 
-    ######################################
-    ############ genotype file ###########
-    ######################################
+    # ######################################
+    # ############ genotype file ###########
+    # ######################################
 
-    #### TO DO: create matrix from plink (no need to expand?)
+    # #### TO DO: create matrix from plink (no need to expand?)
 
-    ## read in genotype file (plink format)
-    G = read_plink1_bin(genotype_file)
+    # ## read in genotype file (plink format)
+    # G = read_plink1_bin(genotype_file)
 
-    ## select relavant SNPs based on feature variant filter file
-    fvf = pd.read_csv(feature_variant_file, index_col=0)
-    leads = fvf[fvf["feature"] == gene_name]["snp_id"].unique()
-    G_sel = G[:, G["snp"].isin(leads)]
+    # ## select relavant SNPs based on feature variant filter file
+    # fvf = pd.read_csv(feature_variant_file, index_col=0)
+    # leads = fvf[fvf["feature"] == gene_name]["snp_id"].unique()
+    # G_sel = G[:, G["snp"].isin(leads)]
 
-    # expand out genotypes from cells to donors (and select relevant donors in the same step)
-    G_expanded = G_sel.sel(sample=sample_mapping["individual_long"].values)
-    # assert all(hK_expanded.sample.values == G_expanded.sample.values)
+    # # expand out genotypes from cells to donors (and select relevant donors in the same step)
+    # G_expanded = G_sel.sel(sample=sample_mapping["individual_long"].values)
+    # # assert all(hK_expanded.sample.values == G_expanded.sample.values)
 
-    # delete large files to free up memory
-    del G
-    del G_sel
+    # # delete large files to free up memory
+    # del G
+    # del G_sel
 
     ### ouputs: y.c, Z, K (optional: X)
 
