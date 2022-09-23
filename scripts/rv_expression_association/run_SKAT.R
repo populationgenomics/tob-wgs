f259bea980d7b52ac4accedd05aef9c41daf79b0
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-09-23 15:52:54 +1000

    simplify R script to get already generated files

--- scripts/rv_expression_association/run_SKAT.R
+++ scripts/rv_expression_association/run_SKAT.R
@@ -10,28 +10,35 @@ gcs_auth(token = token)
 # set bucket
 googleCloudStorageR::gcs_global_bucket("gs://cpg-tob-wgs-test")
 
-# get genotype files (plink format)
-File.Bed <- googleCloudStorageR::gcs_get_object("v0/plink_files/igll5_rare_regulatory.bed")
-File.Bim <- googleCloudStorageR::gcs_get_object("v0/plink_files/igll5_rare_regulatory.bim")
-File.Fam <- googleCloudStorageR::gcs_get_object("plink_files/igll5_rare_regulatory.fam")
+# # get genotype files (plink format)
+# File.Bed <- googleCloudStorageR::gcs_get_object("v0/plink_files/igll5_rare_regulatory.bed")
+# File.Bim <- googleCloudStorageR::gcs_get_object("v0/plink_files/igll5_rare_regulatory.bim")
+# File.Fam <- googleCloudStorageR::gcs_get_object("plink_files/igll5_rare_regulatory.fam")
 
-File.SetID <- googleCloudStorageR::gcs_get_object("v0/skat/igll5_rare_regulatory.SetID")
+# File.SetID <- googleCloudStorageR::gcs_get_object("v0/skat/igll5_rare_regulatory.SetID")
 
-File.SSD <- "IGLL5_50K_RV_VEP.SSD"
-File.Info <- "IGLL5_50K_RV_VEP.Info"
+# File.SSD <- "IGLL5_50K_RV_VEP.SSD"
+# File.Info <- "IGLL5_50K_RV_VEP.Info"
 
-# generate SSD file
-ssd <- Generate_SSD_SetID(File.Bed, File.Bim, File.Fam, File.SetID, File.SSD, File.Info)
+# # generate SSD file
+# ssd <- Generate_SSD_SetID(File.Bed, File.Bim, File.Fam, File.SetID, File.SSD, File.Info)
 
-# get expression file (pseudobulk)
-E <- 
+# # get expression file (pseudobulk)
+# E <- 
 
-# get covariates
-X <- 1  # intercept only
+# # get covariates
+# X <- 1  # intercept only
 
-# set up SKAT
-Z <- Get_Genotypes_SSD(ssd)
+# # set up SKAT
+# Z <- Get_Genotypes_SSD(ssd)
+# y.c <- 
+
+
+# get input files generated elsewhere
+Z <- 
 y.c <- 
+K <- 
+X <- 1  # intercept only
 
 # run null model (no Kinship)
 obj <- SKAT_Null_Model(y.c ~ X, out_type = "C")
