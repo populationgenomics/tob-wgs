fa9a67025d4eb2a7e7655a0b75c6b27ef37d54bc
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-09-26 15:05:47 +1000

    comment everything out except pheno to test

--- scripts/rv_expression_association/prepare_inputs.py
+++ scripts/rv_expression_association/prepare_inputs.py
@@ -6,16 +6,16 @@ import scanpy as sc
 import xarray as xr
 # from cloudpathlib import AnyPath
 from cpg_utils import to_path
-from pandas_plink import read_plink1_bin
-from limix.qc import quantile_gaussianize
+# from pandas_plink import read_plink1_bin
+# from limix.qc import quantile_gaussianize
 
 # use logging to print statements, display at info level
 logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
 
 phenotype_file = "gs://cpg-tob-wgs-test/v0/skat/sce22.h5ad"
-genotype_file_bed = "gs://cpg-tob-wgs-main/scrna-seq/CellRegMap_input_files/plink_files/plink_chr22.bed"
-genotype_file_bim = "gs://cpg-tob-wgs-main/scrna-seq/CellRegMap_input_files/plink_files/plink_chr22.bim"
-genotype_file_fam = "gs://cpg-tob-wgs-main/scrna-seq/CellRegMap_input_files/plink_files/plink_chr22.fam"
+# genotype_file_bed = "gs://cpg-tob-wgs-main/scrna-seq/CellRegMap_input_files/plink_files/plink_chr22.bed"
+# genotype_file_bim = "gs://cpg-tob-wgs-main/scrna-seq/CellRegMap_input_files/plink_files/plink_chr22.bim"
+# genotype_file_fam = "gs://cpg-tob-wgs-main/scrna-seq/CellRegMap_input_files/plink_files/plink_chr22.fam"
 
 
 def main(
@@ -34,21 +34,21 @@ def main(
     ##### sample mapping file (SMF) #####
     #####################################
 
-    # this file will map cells to donors
-    sample_mapping = pd.read_csv(
-        sample_mapping_file,
-        dtype={
-            "individual_long": str,
-            "genotype_individual_id": str,
-            "phenotype_sample_id": str,
-        },
-        index_col=0,
-    )
+    # # this file will map cells to donors
+    # sample_mapping = pd.read_csv(
+    #     sample_mapping_file,
+    #     dtype={
+    #         "individual_long": str,
+    #         "genotype_individual_id": str,
+    #         "phenotype_sample_id": str,
+    #     },
+    #     index_col=0,
+    # )
 
-    ## extract unique individuals
-    donors0 = sample_mapping["genotype_individual_id"].unique()
-    donors0.sort()
-    logging.info("Number of unique donors: {}".format(len(donors0)))
+    # ## extract unique individuals
+    # donors0 = sample_mapping["genotype_individual_id"].unique()
+    # donors0.sort()
+    # logging.info("Number of unique donors: {}".format(len(donors0)))
 
     ######################################
     ########### phenotype file ###########
@@ -93,26 +93,26 @@ def main(
     # ############ kinship file ############
     # ######################################
 
-    ## read in GRM (genotype relationship matrix; kinship matrix)
-    K = pd.read_csv(kinship_file, index_col=0)
-    K.index = K.index.astype("str")
-    assert all(K.columns == K.index)  # symmetric matrix, donors x donors
+    # ## read in GRM (genotype relationship matrix; kinship matrix)
+    # K = pd.read_csv(kinship_file, index_col=0)
+    # K.index = K.index.astype("str")
+    # assert all(K.columns == K.index)  # symmetric matrix, donors x donors
 
-    K = xr.DataArray(
-        K.values,
-        dims=["sample_0", "sample_1"],
-        coords={"sample_0": K.columns, "sample_1": K.index},
-    )
-    K = K.sortby("sample_0").sortby("sample_1")
-    donors = sorted(set(list(K.sample_0.values)).intersection(donors0))
-    logging.info("Number of donors after kinship intersection: {}".format(len(donors)))
+    # K = xr.DataArray(
+    #     K.values,
+    #     dims=["sample_0", "sample_1"],
+    #     coords={"sample_0": K.columns, "sample_1": K.index},
+    # )
+    # K = K.sortby("sample_0").sortby("sample_1")
+    # donors = sorted(set(list(K.sample_0.values)).intersection(donors0))
+    # logging.info("Number of donors after kinship intersection: {}".format(len(donors)))
 
-    ## subset to relevant donors
-    K = K.sel(sample_0=donors, sample_1=donors)
-    assert all(K.sample_0 == donors)
-    assert all(K.sample_1 == donors)
+    # ## subset to relevant donors
+    # K = K.sel(sample_0=donors, sample_1=donors)
+    # assert all(K.sample_0 == donors)
+    # assert all(K.sample_1 == donors)
 
-    del K  # delete K to free up memory
+    # del K  # delete K to free up memory
 
     # ######################################
     # ############ genotype file ###########
@@ -122,36 +122,36 @@ def main(
 
     ## read in genotype file (plink format)
     # bed
-    with to_path(genotype_file_bed).open('rb') as handle:
-        data = handle.readlines()
-    with open('temp.bed', 'wb') as handle:
-        handle.writelines(data)
-    # bim
-    with to_path(genotype_file_bim).open('rb') as handle:
-        data = handle.readlines()
-    with open('temp.bim', 'wb') as handle:
-        handle.writelines(data)
-    # fam
-    with to_path(genotype_file_fam).open('rb') as handle:
-        data = handle.readlines()
-    with open('temp.fam', 'wb') as handle:
-        handle.writelines(data)
-    # read
-    G = read_plink1_bin('temp.bed')
+    # with to_path(genotype_file_bed).open('rb') as handle:
+    #     data = handle.readlines()
+    # with open('temp.bed', 'wb') as handle:
+    #     handle.writelines(data)
+    # # bim
+    # with to_path(genotype_file_bim).open('rb') as handle:
+    #     data = handle.readlines()
+    # with open('temp.bim', 'wb') as handle:
+    #     handle.writelines(data)
+    # # fam
+    # with to_path(genotype_file_fam).open('rb') as handle:
+    #     data = handle.readlines()
+    # with open('temp.fam', 'wb') as handle:
+    #     handle.writelines(data)
+    # # read
+    # G = read_plink1_bin('temp.bed')
     
-    ## select relavant SNPs based on feature variant filter file
-    fvf = pd.read_csv(feature_variant_file, index_col=0)
-    set_variants = fvf[fvf["feature"] == gene_name]["snp_id"].unique()
-    G_sel = G[:, G["snp"].isin(set_variants)]
+    # ## select relavant SNPs based on feature variant filter file
+    # fvf = pd.read_csv(feature_variant_file, index_col=0)
+    # set_variants = fvf[fvf["feature"] == gene_name]["snp_id"].unique()
+    # G_sel = G[:, G["snp"].isin(set_variants)]
 
-    Z = G_sel.sel(sample=sample_mapping["individual_long"].values)
-    # expand out genotypes from cells to donors (and select relevant donors in the same step)
-    # G_expanded = G_sel.sel(sample=sample_mapping["individual_long"].values)
-    # assert all(hK_expanded.sample.values == G_expanded.sample.values)
+    # Z = G_sel.sel(sample=sample_mapping["individual_long"].values)
+    # # expand out genotypes from cells to donors (and select relevant donors in the same step)
+    # # G_expanded = G_sel.sel(sample=sample_mapping["individual_long"].values)
+    # # assert all(hK_expanded.sample.values == G_expanded.sample.values)
 
-    # delete large files to free up memory
-    del G
-    del G_sel
+    # # delete large files to free up memory
+    # del G
+    # del G_sel
 
     ### ouputs: y.c, Z, K (optional: X)
 
