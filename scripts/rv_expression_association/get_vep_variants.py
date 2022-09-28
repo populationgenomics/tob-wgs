4e811fd71c166500b3d89d6e2c8fbb5da12930e5
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-09-28 11:19:25 +1000

    please lint

--- scripts/rv_expression_association/get_vep_variants.py
+++ scripts/rv_expression_association/get_vep_variants.py
@@ -5,7 +5,7 @@ import hail as hl
 import numpy as np
 import pandas as pd
 from hail.methods import export_plink
-from cpg_utils.hail_batch import dataset_path, init_batch, output_path 
+from cpg_utils.hail_batch import dataset_path, init_batch, output_path
 
 # object containing variants within a 50K window on either side of the VPREB3 gene
 MT = dataset_path('v0/VPREB3_50K_window.mt')
@@ -13,6 +13,7 @@ VEP_HT = dataset_path('v0/VPREB3_50K_window_vep.ht')
 
 # use logging to print statements, display at info level
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+
 
 def main():
     # read and densify object
@@ -43,37 +44,55 @@ def main():
     logging.info('Number of rare variants (freq<5%): {}'.format(rv_mt.count()[0]))
 
     # filter variants found to have regulatory effects
-    filtered_mt = rv_mt.filter_rows(hl.len(rv_mt.vep.regulatory_feature_consequences['biotype']) > 0)
-    logging.info('Number of rare variants (freq<5%) with ergulatory conequences: {}'.format(filtered_mt.count()[0]))
+    filtered_mt = rv_mt.filter_rows(
+        hl.len(rv_mt.vep.regulatory_feature_consequences['biotype']) > 0
+    )
+    logging.info(
+        'Number of rare variants (freq<5%) with ergulatory conequences: {}'.format(
+            filtered_mt.count()[0]
+        )
+    )
 
     filtered_rrv_mt = filtered_mt.filter_rows(filtered_mt.variant_qc.AF[1] < 0.01)
-    logging.info('Number of rarer variants (freq<1%): {}'.format(filtered_rrv_mt.count()[0]))
+    logging.info(
+        'Number of rarer variants (freq<1%): {}'.format(filtered_rrv_mt.count()[0])
+    )
 
     filtered_0maf_mt = filtered_mt.filter_rows(filtered_mt.variant_qc.AF[1] == 0)
-    logging.info('Check that there are {} variants with freq==0'.format(filtered_0maf_mt.count()[0]))
+    logging.info(
+        'Check that there are {} variants with freq==0'.format(
+            filtered_0maf_mt.count()[0]
+        )
+    )
 
     # print out stats
     # types of regulatory variants
-    biotypes = pd.Series(filtered_mt.vep.regulatory_feature_consequences['biotype'].collect())
-    print(biotypes.value_counts())
+    biotypes = pd.Series(
+        filtered_mt.vep.regulatory_feature_consequences['biotype'].collect()
+    )
+    print(
+        biotypes.value_counts()
+    )  # not sure how to turn this into logging? Multiline table
 
     # MAF distribution
     mafs = pd.Series(filtered_mt.variant_qc.AF[1].collect())
-    print(np.nanmin(mafs))
-    print(np.nanmax(mafs))
-    print(np.nanmean(mafs))
+    logging.info('Min frequency in set: {}'.format(np.nanmin(mafs)))
+    logging.info('Max frequency in set: {}'.format(np.nanmax(mafs)))
+    logging.info('Mean frequency in set: {}'.format(np.nanmean(mafs)))
 
     # export MT object to PLINK (all regulatory variants)
     filtered_mt_path = output_path('plink_files/vpreb3_rare_regulatory')
-    export_plink(filtered_mt, filtered_mt_path, ind_id = filtered_mt.s)
+    export_plink(filtered_mt, filtered_mt_path, ind_id=filtered_mt.s)
 
     # filter to promoter variants only
-    filtered_mt2 = filtered_mt.filter_rows(filtered_mt.vep.regulatory_feature_consequences['biotype'][0] == 'promoter')
+    filtered_mt2 = filtered_mt.filter_rows(
+        filtered_mt.vep.regulatory_feature_consequences['biotype'][0] == 'promoter'
+    )
     print(filtered_mt2.count())
     # export to PLINK
     filtered_mt_path = output_path('plink_files/vpreb3_rare_promoter')
-    export_plink(filtered_mt2, filtered_mt_path, ind_id = filtered_mt2.s)
-    
+    export_plink(filtered_mt2, filtered_mt_path, ind_id=filtered_mt2.s)
+
 
 if __name__ == '__main__':
     main()
