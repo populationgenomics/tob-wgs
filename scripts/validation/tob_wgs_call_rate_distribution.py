a6555776614bd6626aa51a9a232b888c4d5a4c79
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-09-29 14:14:35 +1000

    Add in cumulative distribution plot

--- scripts/validation/tob_wgs_call_rate_distribution.py
+++ scripts/validation/tob_wgs_call_rate_distribution.py
@@ -22,12 +22,16 @@ def main():
     # filter out variants that didn't pass the VQSR filter
     mt = mt.filter_rows(hl.len(hl.or_else(mt.filters, hl.empty_set(hl.tstr))) == 0)
     # Calculate call rate and add to mt
-    mt = mt.annotate_rows(call_rate=hl.agg.sum(hl.is_defined(mt.GT)) / n_samples))
+    mt = mt.annotate_rows(call_rate=hl.agg.sum(hl.is_defined(mt.GT)) / n_samples)
     # plot distribution
-    p = hl.plot.histogram(mt.call_rate, range=(0, 1), bins=30, legend='Call Rate', title='Call Rate Distribution')
-    p_filename = output_path('histogram_call_rate.png', 'web')
-    with hl.hadoop_open(p_filename, 'wb') as f:
-        get_screenshot_as_png(p).save(f, format='PNG')
+    p1 = hl.plot.histogram(mt.call_rate, range=(0, 1), bins=30, legend='Call Rate', title='Call Rate Distribution')
+    p1_filename = output_path('histogram_call_rate.png', 'web')
+    with hl.hadoop_open(p1_filename, 'wb') as f:
+        get_screenshot_as_png(p1).save(f, format='PNG')
+    p2 = hl.plot.cumulative_histogram(mt.call_rate, range=(0, 1), bins=30, legend='Call Rate', title='Call Rate Distribution')
+    p2_filename = output_path('cumulative)histogram_call_rate.png', 'web')
+    with hl.hadoop_open(p2_filename, 'wb') as f:
+        get_screenshot_as_png(p2).save(f, format='PNG')
 
 
 if __name__ == '__main__':
