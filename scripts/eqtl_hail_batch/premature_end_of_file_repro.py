39477216b5b59e8ef72eecfa9c609c1e17c33886
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-10-17 12:18:40 +1100

    Linter

--- scripts/eqtl_hail_batch/premature_end_of_file_repro.py
+++ scripts/eqtl_hail_batch/premature_end_of_file_repro.py
@@ -2,6 +2,7 @@
 
 """Reproduction case for the Hail team."""
 
+import hail as hl
 from cpg_utils.hail_batch import (
     dataset_path,
     init_batch,
@@ -12,6 +13,7 @@ MT_PATH = dataset_path('mt/v7.mt')
 NUM_PARTITIONS = 10000
 
 def main():
+    """Main entrypoint."""
     init_batch()
     mt = hl.read_matrix_table(MT_PATH)
     intervals = mt._calculate_new_partitions(NUM_PARTITIONS)
