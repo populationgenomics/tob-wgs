eb917359902b8ee4c3f9077d1cbfccc9ed050fd9
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-10-17 12:23:10 +1100

    Linter³

--- scripts/eqtl_hail_batch/premature_end_of_file_repro.py
+++ scripts/eqtl_hail_batch/premature_end_of_file_repro.py
@@ -12,6 +12,7 @@ from cpg_utils.hail_batch import (
 MT_PATH = dataset_path('mt/v7.mt')
 NUM_PARTITIONS = 10000
 
+
 def main():
     """Main entrypoint."""
     init_batch()
@@ -20,6 +21,7 @@ def main():
     intervals_path = output_path(f'eqtl/debug/intervals_{NUM_PARTITIONS}', 'analysis')
     hl.experimental.write_expression(intervals, intervals_path)
     print(hl.read_matrix_table(MT_PATH, _intervals=intervals).rows()._force_count())  # pylint: disable=protected-access
+
 
 if __name__ == '__main__':
     main()
