4e5dc78e40d568c772a2f600daaaa6a7e82e0a9b
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-10-17 14:05:04 +1100

    Test if we can reproduce the crash with `naive_coalesce` directly (#143)

--- scripts/eqtl_hail_batch/premature_end_of_file_repro.py
+++ scripts/eqtl_hail_batch/premature_end_of_file_repro.py
@@ -3,11 +3,7 @@
 """Reproduction case for the Hail team."""
 
 import hail as hl
-from cpg_utils.hail_batch import (
-    dataset_path,
-    init_batch,
-    output_path
-)
+from cpg_utils.hail_batch import dataset_path, init_batch
 
 MT_PATH = dataset_path('mt/v7.mt')
 NUM_PARTITIONS = 10000
@@ -17,10 +13,8 @@ def main():
     """Main entrypoint."""
     init_batch()
     mt = hl.read_matrix_table(MT_PATH)
-    intervals = mt._calculate_new_partitions(NUM_PARTITIONS)  # pylint: disable=protected-access
-    intervals_path = output_path(f'eqtl/debug/intervals_{NUM_PARTITIONS}', 'analysis')
-    hl.experimental.write_expression(intervals, intervals_path)
-    print(hl.read_matrix_table(MT_PATH, _intervals=intervals).rows()._force_count())  # pylint: disable=protected-access
+    mt = mt.naive_coalesce(NUM_PARTITIONS)
+    print(mt.rows()._force_count())  # pylint: disable=protected-access
 
 
 if __name__ == '__main__':
