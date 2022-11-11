eed75b528e08cc0c4d8a7383f83c2f843924b536
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-11-11 15:25:59 +1100

    add to runner

--- scripts/rv_expression_association/pseudobulk_association/run_CellRegMap_RV.py
+++ scripts/rv_expression_association/pseudobulk_association/run_CellRegMap_RV.py
@@ -4,7 +4,7 @@
 # This script runs our CellRegMap-RV method to test for an association between a set of 
 # rare genetic variants (within the promoter of a given gene)
 # and the expression of the gene itself (in one cell type, and aggregated across donors). 
-# The tests performed are a variance-component test, a burden test and an omnibus test
+# The tests performed are a variance-component test, a burden test (max) and an omnibus test
 # The resulting association p-values are recorded.
 
 # import python modules
@@ -30,3 +30,22 @@ from cellregmap import (
     run_burden_association,
     omnibus_set_association,
 )
+
+def get_crm_pvs(pheno, covs, genotypes, E=None):
+    """
+    CellRegMap-RV tests
+    * score test (variance)
+    * burden test (max)
+    * omnibus (Cauchy) test
+    """
+    pv0 = run_gene_set_association(y=pheno, G=genotypes, W=covs, E=E)[0]
+    pv1 = run_burden_association(y=pheno, G=genotypes, W=covs, E=E, mask="mask.max")[0]
+    pv2 = omnibus_set_association(np.array([pv0, pv1]))
+    return [pv0, pv1, pv2]
+
+def main():  
+    
+
+
+if __name__ == '__main__':
+    main()
