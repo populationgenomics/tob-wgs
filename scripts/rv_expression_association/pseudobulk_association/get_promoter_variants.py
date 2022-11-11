f246d3d3adb3bc69cbd0b21984fa88f7eab54c50
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-11-11 14:04:34 +1100

    initial commit pseudobulk association analysis

---
+++ scripts/rv_expression_association/pseudobulk_association/get_promoter_variants.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3  
+# pylint: disable=missing-module-docstring,missing-function-docstring
+
+# For a given gene, this script aims to export all variants (non-ref)
+# from the TOB-WGS dataset that are 
+# * in and around the gene (+- a given window size)
+# * in promoter region (as annotated by VEP)
+# * biallelic SNVs
+# * rare (MAF < 5%)
+
+import click
+import logging
+import hail as hl
+from hail.methods import export_plink
+from cpg_utils.hail_batch import dataset_path, init_batch, output_path
+
+# object containing variants within a 50K window on either side of the VPREB3 gene
+MT = dataset_path('mt/v7.mt')
+VEP_HT = dataset_path('tob_wgs_vep/104/vep104.3_GRCh38.ht')
+
+# use logging to print statements, display at info level
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+
+@click.command()
+@click.option('--chrom', required=True)        # e.g., 22
+@click.option('--gene-name', required=True)    # e.g., VPREB3
+@click.option('--window-size', required=True)  # 10000
+def main(
+    chrom: str,
+    gene_name: str;
+    window_size: int;
+): 
+    # read hail matrix table object (WGS data)
+    init_batch()
+    mt = hl.read_matrix_table(MT)
+    # subset to chromosome
+    mt = 
+    # subset to window
+    mt = 
+    logging.info(f'Number of variants in window: {mt.count()[0]}')
+
+    # densify
+    mt = hl.experimental.densify(mt)
+
+    # filter out low quality variants and consider biallelic variants only (no multi-allelic, no ref-only)
+    mt = mt.filter_rows(
+        (hl.len(hl.or_else(mt.filters, hl.empty_set(hl.tstr))) == 0)  # QC
+        & (hl.len(mt.alleles) == 2)                                   # remove hom-ref
+        & (mt.n_unsplit_alleles == 2)                                 # biallelic
+        & (hl.is_snp(mt.alleles[0], mt.alleles[1]))                   # SNVs
+    )
+
+    # annotate using VEP
+    vep_ht = hl.read_table(VEP_HT)
+    mt = mt.annotate_rows(vep=vep_ht[mt.row_key].vep)
+    mt_path = output_path('densified_qced_snps_only_vep_annotated.mt', 'tmp')
+    mt = mt.checkpoint(
+        mt_path, overwrite=True
+    )  # add checkpoint to avoid repeat evaluation
+    logging.info(f'Number of QC-passing, biallelic SNPs: {mt.count()[0]}')
+
+    # filter rare variants only (freq < 5%)
+    mt = hl.variant_qc(mt)
+    mt = mt.filter_rows((mt.variant_qc.AF[1] < 0.05) & (mt.variant_qc.AF[1] > 0))
+    mt_path = output_path('rare_variants.mt', 'tmp')
+    mt = mt.checkpoint(rv_mt_path, overwrite=True)  # checkpoint
+    logging.info(f'Number of rare variants (freq<5%): {mt.count()[0]}')
+
+    # filter variants found to be in promoter regions
+    mt = filtered_mt.filter_rows(
+        filtered_mt.vep.regulatory_feature_consequences['biotype'][0] == 'promoter'
+    )
+    filtered_mt_path = output_path('regulatory_variants.mt', 'tmp')
+    filtered_mt = filtered_mt.checkpoint(filtered_mt_path, overwrite=True)  # checkpoint
+    logging.info(
+        f'Number of rare variants (freq<5%) with regulatory consequences: {filtered_mt.count()[0]}'
+    )
+
+    # export this as a Hail table for downstream analysis
+    ht_filename = output_path('vpreb3_rare_regulatory_summary.ht')
+    ht = filtered_mt.rows()
+    ht.write(ht_filename)  
+
+    # export MT object to PLINK (all regulatory variants)
+    filtered_mt_path = output_path('plink_files/vpreb3_rare_regulatory')
+    export_plink(filtered_mt, filtered_mt_path, ind_id=filtered_mt.s)
+
+    # filter to promoter variants only
+    filtered_mt2 = filtered_mt.filter_rows(
+        filtered_mt.vep.regulatory_feature_consequences['biotype'][0] == 'promoter'
+    )
+    # export to PLINK
+    filtered_mt_path2 = output_path('plink_files/vpreb3_rare_promoter')
+    export_plink(filtered_mt2, filtered_mt_path2, ind_id=filtered_mt2.s)
+
+
+if __name__ == '__main__':
+    main()  # pylint: disable=no-value-for-parameter
