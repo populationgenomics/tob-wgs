39a48391382260a5841d06213e8fbfb4ff9f066c
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-11-23 16:15:10 +1100

    Copy geneloc files 7 and 22, as well as genotype data
    Copy geneloc file 7 to run for test gene CNTNAP2, as well as chr22 geneloc file for reference. Additionally, copy filtered genotype file for analysis

---
+++ scripts/data_transfer/copy_geneloc_genotype_files.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# copy chromosome 7 and 22 geneloc files fmor main to test
+gsutil cp gs://cpg-tob-wgs-main/scrna-seq/grch38_association_files/gene_location_files/GRCh38_geneloc_chr7.tsv gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/gene_location_files/
+gsutil cp gs://cpg-tob-wgs-main/scrna-seq/grch38_association_files/gene_location_files/GRCh38_geneloc_chr22.tsv gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/gene_location_files/
+
+# copy genotype output from eQTL association pipeline into analysis to explore data
+gs://cpg-tob-wgs-main-tmp/scrna-seq/eqtl_output/v3-scale-test/genotype_table.mt gs://cpg-tob-wgs-main-tmp/scrna-seq/eqtl_output/v3-scale-test/
