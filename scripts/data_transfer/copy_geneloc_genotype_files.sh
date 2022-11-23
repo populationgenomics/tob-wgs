01ea4dd9c91fc55a83316c4a5d3b2bc5bb923f0a
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-11-23 16:29:46 +1100

    Update scripts/data_transfer/copy_geneloc_genotype_files.sh
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>

--- scripts/data_transfer/copy_geneloc_genotype_files.sh
+++ scripts/data_transfer/copy_geneloc_genotype_files.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-# copy chromosome 7 and 22 geneloc files fmor main to test
+# copy chromosome 7 and 22 geneloc files from main to test
 gsutil cp gs://cpg-tob-wgs-main/scrna-seq/grch38_association_files/gene_location_files/GRCh38_geneloc_chr7.tsv gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/gene_location_files/
 gsutil cp gs://cpg-tob-wgs-main/scrna-seq/grch38_association_files/gene_location_files/GRCh38_geneloc_chr22.tsv gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/gene_location_files/
 
