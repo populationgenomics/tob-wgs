203eb5519e094d6aa8b58f99051a02e8469756ac
Katalina Bobowik <katalina.bobowik@populationgenomics.org.au>
2022-11-23 16:42:22 +1100

    Update scripts/data_transfer/copy_geneloc_genotype_files.sh
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>

--- scripts/data_transfer/copy_geneloc_genotype_files.sh
+++ scripts/data_transfer/copy_geneloc_genotype_files.sh
@@ -5,6 +5,3 @@ set -ex
 # copy chromosome 7 and 22 geneloc files from main to test
 gsutil cp gs://cpg-tob-wgs-main/scrna-seq/grch38_association_files/gene_location_files/GRCh38_geneloc_chr7.tsv gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/gene_location_files/
 gsutil cp gs://cpg-tob-wgs-main/scrna-seq/grch38_association_files/gene_location_files/GRCh38_geneloc_chr22.tsv gs://cpg-tob-wgs-test/scrna-seq/grch38_association_files/gene_location_files/
-
-# copy genotype output from eQTL association pipeline into analysis to explore data
-gs://cpg-tob-wgs-main-tmp/scrna-seq/eqtl_output/v3-scale-test/genotype_table.mt gs://cpg-tob-wgs-main-tmp/scrna-seq/eqtl_output/v3-scale-test/
