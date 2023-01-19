172964ff31bdd8da988300a0c41808f312dbde3e
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2023-01-19 14:09:41 +1100

    attempt at fixing path syntax

--- scripts/rv_expression_association/subset_variants.py
+++ scripts/rv_expression_association/subset_variants.py
@@ -3,19 +3,21 @@
 
 import click
 import hail as hl
-from cpg_utils.hail_batch import dataset_path, init_batch
+from cpg_utils.hail_batch import dataset_path, init_batch, output_path
 
 
 @click.command()
 @click.option('--input-mt-path', required=True)  # 'mt/v7.mt'
 @click.option('--genes', required=True)  # 'LMNA'
-@click.option(
-    '--output-mt-path', required=True
-)  # 'gs://cpg-tob-wgs-main-analysis/tob_wgs_rv/pseudobulk_rv_association/significant_genes_burden_max.mt'
+@click.option('--output=mt-name', required=True)  # 'significant_genes_burden_max.mt'
+# @click.option(
+#     '--output-mt-path', required=True
+# )  # 'gs://cpg-tob-wgs-main-analysis/tob_wgs_rv/pseudobulk_rv_association/significant_genes_burden_max.mt'
 def subset_variants(
     input_mt_path: str,
     genes: str,
-    output_mt_path: str,
+    # output_mt_path: str,
+    output_mt_name: str,
 ):
     init_batch()
 
@@ -23,7 +25,8 @@ def subset_variants(
     variants = []
     genes_of_interest = genes.split(' ')
     for gene in genes_of_interest:
-        ht_object_filename = f'gs://cpg-tob-wgs-test-analysis/tob_wgs_rv/pseudobulk_rv_association/summary_hts/{gene}_rare_promoter_summary.ht'
+        # ht_object_filename = f'gs://cpg-tob-wgs-test-analysis/tob_wgs_rv/pseudobulk_rv_association/summary_hts/{gene}_rare_promoter_summary.ht'
+        ht_object_filename = dataset_path(f'tob_wgs_rv/pseudobulk_rv_association/summary_hts/{gene}_rare_promoter_summary.ht', 'analysis')
         ht = hl.read_table(ht_object_filename)
         variants.extend(ht.locus.collect())  # I am nearly sure the syntax is wrong here
 
@@ -35,6 +38,7 @@ def subset_variants(
     mt = mt.filter_rows(mt.locus in variants)
 
     # save mt
+    output_mt_path = output_path(output_mt_name, 'analysis')
     mt.write(
         output_mt_path, overwrite=True
     )  # how do I specify the analysis bucket again?
