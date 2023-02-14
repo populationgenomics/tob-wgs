44ea925705d5be8b79311b71be8ca36b8ea48135
Anna Cuomo <annacuomo92@gmail.com>
2023-02-14 17:04:44 +1100

    Create copy_atac_annos.sh (#158)
    * Create copy_atac_annos.sh
    
    * add more granular cell type definitions also
    
    * Update scripts/data_transfer/copy_atac_annos.sh
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
    
    ---------
    
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>

---
+++ scripts/data_transfer/copy_atac_annos.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -ex
+
+gsutil -m mv "gs://cpg-tob-wgs-main-upload/open_chromatin_annotation/predicted_l*_celltypes_avg_peaks.csv" gs://cpg-tob-wgs-main/tob_wgs_rv/open_chromatin_annotation/
+
+gsutil -m mv gs://cpg-tob-wgs-main-upload/open_chromatin_annotation/predicted_l1_celltypes_avg_peaks_chr21.csv gs://cpg-tob-wgs-test/tob_wgs_rv/open_chromatin_annotation/
