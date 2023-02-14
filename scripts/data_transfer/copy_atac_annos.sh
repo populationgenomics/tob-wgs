bf5c74c5dba875c3e411440f56be261d9edfc5c8
Anna Cuomo <annacuomo92@gmail.com>
2023-02-14 15:50:00 +1100

    Update scripts/data_transfer/copy_atac_annos.sh
    Co-authored-by: Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>

--- scripts/data_transfer/copy_atac_annos.sh
+++ scripts/data_transfer/copy_atac_annos.sh
@@ -2,8 +2,6 @@
 
 set -ex
 
-gsutil -m mv gs://cpg-tob-wgs-main-upload/open_chromatin_annotation/predicted_l1_celltypes_avg_peaks.csv gs://cpg-tob-wgs-main/tob_wgs_rv/open_chromatin_annotation/
-gsutil -m mv gs://cpg-tob-wgs-main-upload/open_chromatin_annotation/predicted_l2_celltypes_avg_peaks.csv gs://cpg-tob-wgs-main/tob_wgs_rv/open_chromatin_annotation/
-gsutil -m mv gs://cpg-tob-wgs-main-upload/open_chromatin_annotation/predicted_l3_celltypes_avg_peaks.csv gs://cpg-tob-wgs-main/tob_wgs_rv/open_chromatin_annotation/
+gsutil -m mv "gs://cpg-tob-wgs-main-upload/open_chromatin_annotation/predicted_l*_celltypes_avg_peaks.csv" gs://cpg-tob-wgs-main/tob_wgs_rv/open_chromatin_annotation/
 
 gsutil -m mv gs://cpg-tob-wgs-main-upload/open_chromatin_annotation/predicted_l1_celltypes_avg_peaks_chr21.csv gs://cpg-tob-wgs-test/tob_wgs_rv/open_chromatin_annotation/
