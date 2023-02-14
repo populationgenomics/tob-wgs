af5e4cd227339b55c464584eb4ee1b3fde909a1f
Anna Cuomo <annacuomo92@gmail.com>
2023-02-14 14:29:06 +1100

    add more granular cell type definitions also

--- scripts/data_transfer/copy_atac_annos.sh
+++ scripts/data_transfer/copy_atac_annos.sh
@@ -3,5 +3,7 @@
 set -ex
 
 gsutil -m mv gs://cpg-tob-wgs-main-upload/open_chromatin_annotation/predicted_l1_celltypes_avg_peaks.csv gs://cpg-tob-wgs-main/tob_wgs_rv/open_chromatin_annotation/
+gsutil -m mv gs://cpg-tob-wgs-main-upload/open_chromatin_annotation/predicted_l2_celltypes_avg_peaks.csv gs://cpg-tob-wgs-main/tob_wgs_rv/open_chromatin_annotation/
+gsutil -m mv gs://cpg-tob-wgs-main-upload/open_chromatin_annotation/predicted_l3_celltypes_avg_peaks.csv gs://cpg-tob-wgs-main/tob_wgs_rv/open_chromatin_annotation/
 
 gsutil -m mv gs://cpg-tob-wgs-main-upload/open_chromatin_annotation/predicted_l1_celltypes_avg_peaks_chr21.csv gs://cpg-tob-wgs-test/tob_wgs_rv/open_chromatin_annotation/
