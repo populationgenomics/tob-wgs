c676ae2ab395e2d25bd5fb83c0b25bccc90b2da3
Anna Cuomo <annacuomo92@gmail.com>
2023-02-16 13:38:40 +1100

    fix typo

--- scripts/rv_expression_association/annotate_variants.py
+++ scripts/rv_expression_association/annotate_variants.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# pylint: missing-module-docstring,no-value-for-parameter
+# pylint: disable=missing-module-docstring,no-value-for-parameter
 
 import click
 import hail as hl
