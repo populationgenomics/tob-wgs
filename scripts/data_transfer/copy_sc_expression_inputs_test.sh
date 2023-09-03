be0761fcdbafec4334625b7f25bdd9ec5f33b96e
Anna Cuomo <annacuomo92@gmail.com>
2023-09-04 09:49:07 +1000

    add full paths

--- scripts/data_transfer/copy_sc_expression_inputs_test.sh
+++ scripts/data_transfer/copy_sc_expression_inputs_test.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-gsutil -m cp cpg-tob-wgs-main/scrna-seq/CellRegMap_input_files/expression_objects/sce22.h5ad gs://cpg-tob-wgs-test/scrna-seq/CellRegMap_input_files/expression_objects/
-gsutil -m cp cpg-tob-wgs-main/scrna-seq/CellRegMap_input_files/all_B_cells/PCs_Bcells.csv.pkl gs://cpg-tob-wgs-test/scrna-seq/CellRegMap_input_files/expression_PCs/
+gsutil -m cp gs://cpg-tob-wgs-main/scrna-seq/CellRegMap_input_files/expression_objects/sce22.h5ad gs://cpg-tob-wgs-test/scrna-seq/CellRegMap_input_files/expression_objects/
+gsutil -m cp gs://cpg-tob-wgs-main/scrna-seq/CellRegMap_input_files/all_B_cells/PCs_Bcells.csv.pkl gs://cpg-tob-wgs-test/scrna-seq/CellRegMap_input_files/expression_PCs/
