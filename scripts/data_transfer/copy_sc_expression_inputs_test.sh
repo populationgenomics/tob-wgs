7e48d8529242cf6f843f5c82913e03b2ebcd029a
Anna Cuomo <annacuomo92@gmail.com>
2023-09-04 10:04:29 +1000

    copy input files to test (#166)
    * copy input files to test
    
    * add full paths

---
+++ scripts/data_transfer/copy_sc_expression_inputs_test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -ex
+
+gsutil -m cp gs://cpg-tob-wgs-main/scrna-seq/CellRegMap_input_files/expression_objects/sce22.h5ad gs://cpg-tob-wgs-test/scrna-seq/CellRegMap_input_files/expression_objects/
+gsutil -m cp gs://cpg-tob-wgs-main/scrna-seq/CellRegMap_input_files/all_B_cells/PCs_Bcells.csv.pkl gs://cpg-tob-wgs-test/scrna-seq/CellRegMap_input_files/expression_PCs/
